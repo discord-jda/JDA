b87503f3b2dba815b61154fd7542a024e2091f7b
Austin Keener <keeneraustin@yahoo.com>
2016-01-27 02:03:18 +0900

    Rewrote the ServiceUtils system to properly support JDA in both FatJar and non FatJar situations. Should have done it this way to start with...

--- src/main/java/net/dv8tion/jda/utils/ServiceUtil.java
+++ src/main/java/net/dv8tion/jda/utils/ServiceUtil.java
@@ -17,128 +17,108 @@ package net.dv8tion.jda.utils;
 
 import sun.net.spi.nameservice.dns.DNSNameService;
 
-import javax.sound.midi.spi.MidiDeviceProvider;
 import javax.sound.midi.spi.MidiFileReader;
 import javax.sound.midi.spi.MidiFileWriter;
-import javax.sound.midi.spi.SoundbankReader;
 import javax.sound.sampled.spi.AudioFileReader;
 import javax.sound.sampled.spi.AudioFileWriter;
 import javax.sound.sampled.spi.FormatConversionProvider;
-import javax.sound.sampled.spi.MixerProvider;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
-import java.nio.file.*;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.ServiceLoader;
+import java.nio.file.Files;
+import java.util.*;
+import java.util.jar.JarOutputStream;
+import java.util.zip.ZipEntry;
 
 /**
  * Internal class used to load the Java Service Provider Interfaces used by JDA.
  */
 public class ServiceUtil
 {
+    public static final Map<Class, List<String>> SERVICES;  //Populated at the bottom of this file.
+    public static final String SERVICES_DIRECTORY = "META-INF/services/";
+
     public static void loadServices()
     {
-        URI uri = null;
-        List<Path> dependencyJars = new LinkedList<Path>();
+        File servicesJar = null;
+        FileOutputStream fos = null;
+        JarOutputStream zos = null;
+
         try
         {
-            uri = ServiceUtil.class.getProtectionDomain().getCodeSource().getLocation().toURI();
-            if (uri.toString().endsWith("jar")) //We only care about loading if we are in a Jar. In IDE we should have stuff in classpath.
+            //Creates a new temp file to act as a dummy jar containing only our META-INF/services/ folder.
+            servicesJar = Files.createTempFile("jda-services", "jar").toFile();
+            servicesJar.deleteOnExit();     //Sets to delete when the JVM closes, but it wont because it is loaded into the JVM. :/
+            fos = new FileOutputStream(servicesJar);    //Opens an output stream so we can write to the file
+            zos = new JarOutputStream(fos);             //Wraps the stream so we can treat it like writing to a Jar file
+
+            //Loops through all of our services, creating files in our Jar's META-INF/services/ folder
+            // using the service class's canonical name (package.package.package.classname)
+            //Then writes each provider's canonical name to the created file, 1 per line.
+            for (Map.Entry<Class, List<String>> service : SERVICES.entrySet())
             {
-                //Change from the normal file URI to a jar URI so that we can read use the JDK JAR provider to read the
-                // jar file's internals.
-                String uriMod = "jar:" + uri.toString() + "!/"; //Specifically this sets us at the internal root of the jar
-                URI newUri = new URI(uriMod);
+                String fileName = SERVICES_DIRECTORY + service.getKey().getCanonicalName();
 
-                //Create a virtual file system based on the jar.
-                FileSystem fs = FileSystems.newFileSystem(newUri, Collections.<String, Object>emptyMap());
+                //Creates a new file in the META-INFO/services/ folder in our dummy jar.
+                ZipEntry zipEntry = new ZipEntry(fileName);
 
-                //This returns the root of the jar. We can assume that there exists a root and it is /
-                Path rootPath = fs.getRootDirectories().iterator().next();
+                //Tells the output stream that the provided entry is the one which we are writing to.
+                zos.putNextEntry(zipEntry);
 
-                //Loops through all of the files within the root directory.
-                // We start at a depth of 1 because, techincally, a depth of 0 returns the TRUE root, or the
-                // folder that contains the root folder. We want the internals of the "root" folder, or /
-                // Furthermore, we don't want to search further than the root folder because we are only expecting
-                // dependency jars in the root folder. Setting depth higher would search further into the jar.
-                Files.walk(rootPath, 1).forEach(path ->
+                //Writes each provider's canonical name to this file, 1 per line.
+                for (String provider : service.getValue())
                 {
-                    if(path.toString().endsWith("jar"))
-                    {
-                        dependencyJars.add(path);
-                    }
-                });
-
-                //Creates a temp directory in the OS specific Temporary files folder.
-                Path tempDir = Files.createTempDirectory("JDA-services");
-                tempDir.toFile().deleteOnExit();    //Delete the folder on exit. Resource cleanup.
-
-                //The DNSNameService class is loaded by the Launcher.ExtLoader, also known as the JVM Extension Loader.
-                //This is just a quick shortcut to that specific Loader. The Extension Loader is also the highest loader
-                // accessible by programs. I say "accessible" because techinically there is a higher Classloader,
-                // the Bootstrap loader, however it is inaccessible ([native] code), but even if we could access it
-                // we wouldn't want to in this situation. We specifically need the ExtLoader to load Java Extentions.
-                //I mentioned that this is just a shortcut: The other method of getting the ExtLoader is to
-                // first get the loader of the current class and call .getParent() until you get the ExtLoader. You'll
-                // know that you have the ExtLoader when the .getParent() call returns null. The parent of the ExtLoader
-                // is the Bootstrap Loader which is defined by null.
-                ClassLoader extentionLoader = DNSNameService.class.getClassLoader();
-
-                //Get the "addExtUrl" method from the ExtLoader class. As a note, in the JVM code, this method literally
-                // just calls super.addUrl(URL) which is a URLClassLoader method. Technically we could have go the
-                // addURL(URL) method instead by using getClass().getSuperClass() (which gives us the URLClassLoader
-                // instead of the ExtLoader) however, I chose to use the ExtLoader method because JVM implementation may
-                // change in the future.
-                Method addExtUrl = extentionLoader.getClass().getDeclaredMethod("addExtURL", URL.class);
-
-                //Make the method public accessible.
-                addExtUrl.setAccessible(true);
-
-                //Create temp files for each of our service dependencies inside of the temp directory we created
-                for (Path jarServicePath : dependencyJars)
-                {
-                    //Cuts off the .jar so that it can be properly places as the "suffix" for the temp file
-                    String fileName = jarServicePath.getFileName().toString().replace(".jar", "");
-
-                    //Creates an empty temp file, placing it our temp directory.
-                    Path tempServiceFile = Files.createTempFile(tempDir, fileName, ".jar");
-                    tempServiceFile.toFile().deleteOnExit();
-
-                    //Copies the file our of the jar and into the temp file that we created.
-                    // Earlier we created an empty temp file, so we need to REPLACE_EXISTING.
-                    Path ps = Files.copy(jarServicePath, tempServiceFile, StandardCopyOption.REPLACE_EXISTING);
-
-                    //Give the JVM our service jars to load.
-                    addExtUrl.invoke(extentionLoader, ps.toUri().toURL());
+                    zos.write((provider + "\n").getBytes());
                 }
 
-                //Services that we want to reload.
-                Class[] services = new Class[]{
-                        MixerProvider.class,
-                        FormatConversionProvider.class,
-                        AudioFileReader.class,
-                        AudioFileWriter.class,
-                        MidiDeviceProvider.class,
-                        SoundbankReader.class,
-                        MidiFileWriter.class,
-                        MidiFileReader.class
-                };
+                //We are done writing this service's configuration file. Close it up.
+                zos.closeEntry();
+            }
 
-                //Go through each Service SPI and force it to reload them.
-                //This makes the JVM rescan it's known Extensions, finding ours and loading them.
-                for (Class service : services)
-                {
-                    ServiceLoader.loadInstalled(service); //.forEach(provider -> System.out.println("  - " + provider.getClass().getName()));
-                }
+            //We are done with the Jar file. Close it up!
+            zos.close();
+            fos.close();
+
+            //The DNSNameService class is loaded by the Launcher.ExtLoader, also known as the JVM Extension Loader.
+            //This is just a quick shortcut to that specific Loader. The Extension Loader is also the highest loader
+            // accessible by programs. I say "accessible" because techinically there is a higher Classloader,
+            // the Bootstrap loader, however it is inaccessible ([native] code), but even if we could access it
+            // we wouldn't want to in this situation. We specifically need the ExtLoader to load Java Extentions.
+            //I mentioned that this is just a shortcut: The other method of getting the ExtLoader is to
+            // first get the loader of the current class and call .getParent() until you get the ExtLoader. You'll
+            // know that you have the ExtLoader when the .getParent() call returns null. The parent of the ExtLoader
+            // is the Bootstrap Loader which is defined by null.
+            ClassLoader extentionLoader = DNSNameService.class.getClassLoader();
+
+            //Get the "addExtUrl" method from the ExtLoader class. As a note, in the JVM code, this method literally
+            // just calls super.addUrl(URL) which is a URLClassLoader method. Technically we could have go the
+            // addURL(URL) method instead by using getClass().getSuperClass() (which gives us the URLClassLoader
+            // instead of the ExtLoader) however, I chose to use the ExtLoader method because JVM implementation may
+            // change in the future.
+            Method addExtUrl = extentionLoader.getClass().getDeclaredMethod("addExtURL", URL.class);
+
+            //Make the method public accessible.
+            addExtUrl.setAccessible(true);
+
+
+            //Give the JVM our service jars to load.
+            addExtUrl.invoke(extentionLoader, servicesJar.toURI().toURL());
+
+            //Go through each Service SPI and force it to reload them.
+            //This makes the JVM rescan it's known Extensions, finding ours and loading them.
+            for (Class service : SERVICES.keySet())
+            {
+                ServiceLoader loader = ServiceLoader.load(service);
+                loader.reload();
+                //System.out.println(service.getCanonicalName());
+                //loader.forEach(provider -> System.out.println("  - " + provider.getClass().getName()));
             }
         }
-        catch (URISyntaxException e)
+        catch (FileNotFoundException e)
         {
             e.printStackTrace();
         }
@@ -150,13 +130,78 @@ public class ServiceUtil
         {
             e.printStackTrace();
         }
-        catch (IllegalAccessException e)
-        {
-            e.printStackTrace();
-        }
         catch (InvocationTargetException e)
         {
             e.printStackTrace();
         }
+        catch (IllegalAccessException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                if (zos != null) zos.close();
+                if (fos != null) fos.close();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    static
+    {
+        HashMap<Class, List<String>> services = new HashMap<>();
+
+        services.put(FormatConversionProvider.class,
+            Collections.unmodifiableList(
+                Arrays.asList(
+                    "javazoom.spi.mpeg.sampled.convert.MpegFormatConversionProvider",   //MP3-SPI
+                    "org.tritonus.sampled.convert.UlawFormatConversionProvider",        //Tritonus_Remaining
+                    "org.tritonus.sampled.convert.AlawFormatConversionProvider",        //Tritonus_Remaining
+                    "org.tritonus.sampled.convert.PCM2PCMConversionProvider",           //Tritonus_Remaining
+                    "org.tritonus.sampled.convert.SampleRateConversionProvider",        //Tritonus_Remaining
+                    "org.tritonus.sampled.convert.ImaAdpcmFormatConversionProvider"     //Tritonus_Remaining
+                )
+            )
+        );
+        services.put(AudioFileReader.class,
+            Collections.unmodifiableList(
+                Arrays.asList(
+                    "javazoom.spi.mpeg.sampled.file.MpegAudioFileReader",               //MP3-SPI
+                    "org.tritonus.sampled.file.AuAudioFileReader",                      //Tritonus_Remaining
+                    "org.tritonus.sampled.file.WaveAudioFileReader",                    //Tritonus_Remaining
+                    "org.tritonus.sampled.file.AiffAudioFileReader"                     //Tritonus_Remaining
+                )
+            )
+        );
+        services.put(AudioFileWriter.class,
+            Collections.unmodifiableList(
+                Arrays.asList(
+                    "org.tritonus.sampled.file.AuAudioFileWriter",                      //Tritonus_Remaining
+                    "org.tritonus.sampled.file.WaveAudioFileWriter",                    //Tritonus_Remaining
+                    "org.tritonus.sampled.file.AiffAudioFileWriter"                     //Tritonus_Remaining
+                )
+            )
+        );
+        services.put(MidiFileWriter.class,
+            Collections.unmodifiableList(
+                Arrays.asList(
+                    "org.tritonus.midi.file.StandardMidiFileWriter"                     //Tritonus_Remaining
+                )
+            )
+        );
+        services.put(MidiFileReader.class,
+            Collections.unmodifiableList(
+                Arrays.asList(
+                    "org.tritonus.midi.file.StandardMidiFileReader"                     //Tritonus_Remaining
+                )
+            )
+        );
+
+        SERVICES = Collections.unmodifiableMap(services);
     }
 }
