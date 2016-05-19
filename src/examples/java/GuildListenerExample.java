1f7fac9e087b783a0bb8883de7b61c550bac6a7e
AmberSienna <AmberSienna@users.noreply.github.com>
2016-05-19 04:24:04 +0100

    Removed old references to the Email/Pass login system in Examples (#56)
    * Remove mentions of the old Username/Password combination for login.
    
    * Fixed typo from removal of Username/Password.

--- src/examples/java/GuildListenerExample.java
+++ src/examples/java/GuildListenerExample.java
@@ -46,11 +46,11 @@ public class GuildListenerExample extends ListenerAdapter
         }
         catch (IllegalArgumentException e)
         {
-            System.out.println("The config was not populated. Please enter an email and password.");
+            System.out.println("The config was not populated. Please enter a bot token.");
         }
         catch (LoginException e)
         {
-            System.out.println("The provided email / password combination was incorrect. Please provide valid details.");
+            System.out.println("The provided bot token was incorrect. Please provide valid details.");
         }
         catch (InterruptedException e)
         {
