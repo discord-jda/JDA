12757a62d23a7f3b4f7e232fb03e8bceaa777264
Austin Keener <keeneraustin@yahoo.com>
2016-07-29 02:18:09 -0400

    Expanded on the JDA interface, adding Status from 2.0. Added asBot and asClient to JDA. Created verifyToken in JDAImpl; Checks token to be sure it is valid and it is the proper AccountType. Added AccountTypeException. Self Explanatory.

--- src/main/java/net/dv8tion/jda/core/JDA.java
+++ src/main/java/net/dv8tion/jda/core/JDA.java
@@ -16,7 +16,64 @@
 
 package net.dv8tion.jda.core;
 
+import net.dv8tion.jda.bot.JDABot;
+import net.dv8tion.jda.client.JDAClient;
+import net.dv8tion.jda.core.exceptions.AccountTypeException;
+
 public interface JDA
 {
+    /**
+     * The current status of the JDA instance.
+     */
+    enum Status
+    {
+        /**JDA is currently setting up supporting systems like the AudioSystem.*/
+        INITIALIZING,
+        /**JDA has finished setting up supporting systems and is ready to log in.*/
+        INITIALIZED,
+        /**JDA is currently attempting to log in.*/
+        LOGGING_IN,
+        /**JDA is currently attempting to connect it's websocket to Discord.*/
+        CONNECTING_TO_WEBSOCKET,
+        /**JDA has successfully connected it's websocket to Discord and is populating internal objects.
+         * This process often takes the longest of all Statuses (besides CONNECTED)*/
+        LOADING_SUBSYSTEMS,
+        /**JDA has finished loading everything, is receiving information from Discord and is firing events.*/
+        CONNECTED,
+        /**JDA's main websocket has been disconnected. This <b>DOES NOT</b> mean JDA has shutdown permanently.
+         * This is an in-between status. Most likely ATTEMPTING_TO_RECONNECT or SHUTTING_DOWN/SHUTDOWN will soon follow.*/
+        DISCONNECTED,
+        /**When trying to reconnect to Discord JDA encountered an issue, most likely related to a lack of internet connection,
+         * and is waiting to try reconnecting again.*/
+        WAITING_TO_RECONNECT,
+        /**JDA has been disconnected from Discord and is currently trying to reestablish the connection.*/
+        ATTEMPTING_TO_RECONNECT,
+        /**JDA has received a shutdown request or has been disconnected from Discord and reconnect is disabled, thus,
+         * JDA is in the process of shutting down*/
+        SHUTTING_DOWN,
+        /**JDA has finished shutting down and this instance can no longer be used to communicate with the Discord servers.*/
+        SHUTDOWN,
+        /**While attempting to authenticate, Discord reported that the provided authentication information was invalid.*/
+        FAILED_TO_LOGIN,
+    }
 
+    /**
+     * Gets the current {@link net.dv8tion.jda.core.JDA.Status Status} of the JDA instance.
+     *
+     * @return
+     *      Current JDA status.
+     */
+    Status getStatus();
+
+    AccountType getAccountType();
+
+    default JDAClient asClient()
+    {
+        throw new AccountTypeException(AccountType.CLIENT);
+    }
+
+    default JDABot asBot()
+    {
+        throw new AccountTypeException(AccountType.BOT);
+    }
 }
--- src/main/java/net/dv8tion/jda/core/entities/impl/JDAImpl.java
+++ src/main/java/net/dv8tion/jda/core/entities/impl/JDAImpl.java
@@ -16,12 +16,70 @@
 
 package net.dv8tion.jda.core.entities.impl;
 
+import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.exceptions.AccountTypeException;
+import net.dv8tion.jda.core.exceptions.RateLimitedException;
+import net.dv8tion.jda.core.requests.Request;
+import net.dv8tion.jda.core.requests.RequestBuilder;
+import net.dv8tion.jda.core.requests.Requester;
+import org.apache.http.HttpHost;
+import org.json.JSONObject;
 
 import javax.security.auth.login.LoginException;
 
 public abstract class JDAImpl implements JDA
 {
+    protected Requester requester = new Requester();
+    protected Status status;
+
     public abstract void login(String token) throws LoginException;
 
+    public Status getStatus()
+    {
+        return status;
+    }
+
+    public void setStatus(Status status)
+    {
+        //TODO: Fire status change event.
+        this.status = status;
+    }
+
+    public void verifyToken(String token) throws LoginException
+    {
+        Request request = new RequestBuilder(RequestBuilder.RequestType.GET)
+                .setUrl(Requester.DISCORD_API_PREFIX + "users/@me")
+                .addHeader("authorization", token)
+                .build();
+        Requester.Response response = requester.handle(request);
+        if (response.isOk())
+        {
+            JSONObject json = response.getObject();
+            if (getAccountType() == AccountType.BOT)
+            {
+                if (!json.has("bot") || !json.getBoolean("bot"))
+                    throw new AccountTypeException(AccountType.BOT, "Attempted to login as a BOT with a CLIENT token!");
+            }
+            else
+            {
+                if (json.has("bot") && json.getBoolean("bot"))
+                    throw new AccountTypeException(AccountType.CLIENT, "Attempted to login as a CLIENT with a BOT token!");
+            }
+        }
+        else if (response.isRateLimit())
+            throw new RateLimitedException("auth/login");//TODO: Do more here somehow.
+        else
+        {
+            if (response.code == 401)
+            {
+                throw new LoginException("The provided token was invalid!");
+            }
+            else
+            {
+                throw new RuntimeException("When verifying the authenticity of the provided token, Discord returned an unknown response:\n" +
+                        response.toString());
+            }
+        }
+    }
 }
