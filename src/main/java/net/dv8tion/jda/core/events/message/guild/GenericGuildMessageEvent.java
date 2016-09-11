66c63075dab9807a1f9ea47a876437fdffdf4374
Austin Keener <keeneraustin@yahoo.com>
2016-09-11 03:19:21 -0400

    Added User and Message events from 2.x. Plan to revisit these.

---
+++ src/main/java/net/dv8tion/jda/core/events/message/guild/GenericGuildMessageEvent.java
@@ -0,0 +1,56 @@
+/*
+ *     Copyright 2015-2016 Austin Keener & Michael Ritter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package net.dv8tion.jda.core.events.message.guild;
+
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.TextChannel;
+import net.dv8tion.jda.core.events.message.GenericMessageEvent;
+
+/**
+ * <b><u>GenericGuildMessageEvent</u></b><br>
+ * Fired whenever a {@link net.dv8tion.jda.core.entities.Message Message} event is fired from a {@link net.dv8tion.jda.core.entities.TextChannel TextChannel}.<br>
+ * Every GuildMessageEvent is an instance of this event and can be casted. (no exceptions)<br>
+ * <br>
+ * Use: Detect any GuildMessageEvent. <i>(No real use for the JDA user)</i>
+ */
+public class GenericGuildMessageEvent extends GenericMessageEvent
+{
+    protected TextChannel channel;
+
+    public GenericGuildMessageEvent(JDA api, long responseNumber, Message message, TextChannel channel)
+    {
+        super(api, responseNumber, message);
+        this.channel = channel;
+    }
+
+    public TextChannel getChannel()
+    {
+        return channel;
+    }
+
+    public Guild getGuild()
+    {
+        return channel.getGuild();
+    }
+
+    public Member getMember()
+    {
+        return getGuild().getMember(getAuthor());
+    }
+}
---
+++ src/main/java/net/dv8tion/jda/core/events/user/UserOnlineStatusUpdateEvent.java
@@ -0,0 +1,55 @@
+/*
+ *     Copyright 2015-2016 Austin Keener & Michael Ritter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package net.dv8tion.jda.core.events.user;
+
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.OnlineStatus;
+import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.User;
+
+/**
+ * <b><u>UserOnlineStatusUpdateEvent</u></b><br/>
+ * Fired if the {@link OnlineStatus OnlineStatus} of a {@link net.dv8tion.jda.core.entities.User User} changes.<br/>
+ * <br/>
+ * Use: Retrieve the User who's status changed and their previous status.
+ */
+public class UserOnlineStatusUpdateEvent extends GenericUserEvent
+{
+    private final OnlineStatus previousOnlineStatus;
+    private final Guild guild;
+
+    public UserOnlineStatusUpdateEvent(JDA api, long responseNumber, User user, Guild guild, OnlineStatus previousOnlineStatus)
+    {
+        super(api, responseNumber, user);
+        this.guild = guild;
+        this.previousOnlineStatus = previousOnlineStatus;
+    }
+
+    public OnlineStatus getPreviousOnlineStatus()
+    {
+        return previousOnlineStatus;
+    }
+
+    public Guild getGuild()
+    {
+        return guild;
+    }
+
+    public boolean isRelationshipUpdate()
+    {
+        return getGuild() == null;
+    }
+}
