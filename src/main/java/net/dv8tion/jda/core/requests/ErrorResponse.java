df9c538569be7c5f1fea06cbc331d73df18abbeb
Austin Keener <keeneraustin@yahoo.com>
2017-01-14 00:41:48 -0500

    Added pre-checks for bulk_delete due to Discord restricting deletion to messages that are less than 2 weeks old

--- src/main/java/net/dv8tion/jda/core/requests/ErrorResponse.java
+++ src/main/java/net/dv8tion/jda/core/requests/ErrorResponse.java
@@ -62,6 +62,7 @@ public enum ErrorResponse
     INVALID_MFA_LEVEL(  50017, "Provided MFA level was invalid."),
     INVALID_PASSWORD(   50018, "Provided password was invalid"),
     INVALID_PIN(        50019, "A message can only be pinned to the channel it was sent in"),
+    INVALID_BULK_DELETE_MESSAGE_AGE(50034, "A Message provided to bulk_delete was older than 2 weeks"),
     MFA_NOT_ENABLED(    60003, "MFA auth required but not enabled"),
     REACTION_BLOCKED(   90001, "Reaction Blocked"),
 
