007ed313f02fa733d6edf53b94ef3ac63d7bb2ed
v0idst4r <vst4rbot@gmail.com>
2017-03-05 15:08:01 -0600

    Readable Permission names

--- src/main/java/net/dv8tion/jda/core/Permission.java
+++ src/main/java/net/dv8tion/jda/core/Permission.java
@@ -23,49 +23,61 @@ import java.util.List;
 
 public enum Permission
 {
-    CREATE_INSTANT_INVITE(0, true, true),
-    KICK_MEMBERS(1, true, false),
-    BAN_MEMBERS(2, true, false),
-    ADMINISTRATOR(3, true, false),
-    MANAGE_CHANNEL(4, true, true),
-    MANAGE_SERVER(5, true, false),
-    MESSAGE_ADD_REACTION(6, true, true),
+    CREATE_INSTANT_INVITE(0, true, true, "Create Instant Invite"),
+    KICK_MEMBERS(1, true, false, "Kick Members"),
+    BAN_MEMBERS(2, true, false, "Ban Members"),
+    ADMINISTRATOR(3, true, false, "Administrator"),
+    MANAGE_CHANNEL(4, true, true, "Manage Channels"),
+    MANAGE_SERVER(5, true, false, "Manage Server"),
+    MESSAGE_ADD_REACTION(6, true, true, "Add Reactions"),
 
-    MESSAGE_READ(10, true, true),
-    MESSAGE_WRITE(11, true, true),
-    MESSAGE_TTS(12, true, true),
-    MESSAGE_MANAGE(13, true, true),
-    MESSAGE_EMBED_LINKS(14, true, true),
-    MESSAGE_ATTACH_FILES(15, true, true),
-    MESSAGE_HISTORY(16, true, true),
-    MESSAGE_MENTION_EVERYONE(17, true, true),
-    MESSAGE_EXT_EMOJI(18, true, true),
+    MESSAGE_READ(10, true, true, "Read Messages"),
+    MESSAGE_WRITE(11, true, true, "Send Messages"),
+    MESSAGE_TTS(12, true, true, "Send TTS Messages"),
+    MESSAGE_MANAGE(13, true, true, "Manage Messages"),
+    MESSAGE_EMBED_LINKS(14, true, true, "Embed Links"),
+    MESSAGE_ATTACH_FILES(15, true, true, "Attach Files"),
+    MESSAGE_HISTORY(16, true, true, "Read History"),
+    MESSAGE_MENTION_EVERYONE(17, true, true, "Mention Everyone"),
+    MESSAGE_EXT_EMOJI(18, true, true, "Use External Emojis"),
 
-    VOICE_CONNECT(20, true, true),
-    VOICE_SPEAK(21, true, true),
-    VOICE_MUTE_OTHERS(22, true, true),
-    VOICE_DEAF_OTHERS(23, true, true),
-    VOICE_MOVE_OTHERS(24, true, true),
-    VOICE_USE_VAD(25, true, true),
+    VOICE_CONNECT(20, true, true, "Connect"),
+    VOICE_SPEAK(21, true, true, "Speak"),
+    VOICE_MUTE_OTHERS(22, true, true, "Mute Members"),
+    VOICE_DEAF_OTHERS(23, true, true, "Deafen Members"),
+    VOICE_MOVE_OTHERS(24, true, true, "Move Members"),
+    VOICE_USE_VAD(25, true, true, "Use Voice Activity"),
 
-    NICKNAME_CHANGE(26, true, false),
-    NICKNAME_MANAGE(27, true, false),
+    NICKNAME_CHANGE(26, true, false, "Change Nickname"),
+    NICKNAME_MANAGE(27, true, false, "Manage Nicknames"),
 
-    MANAGE_ROLES(28, true, false),
-    MANAGE_PERMISSIONS(28, false, true),
-    MANAGE_WEBHOOKS(29, true, true),
-    MANAGE_EMOTES(30, true, false),
+    MANAGE_ROLES(28, true, false, "Manage Roles"),
+    MANAGE_PERMISSIONS(28, false, true, "Manage Permissions"),
+    MANAGE_WEBHOOKS(29, true, true, "Manage Webhooks"),
+    MANAGE_EMOTES(30, true, false, "Manage Emojis"),
 
-    UNKNOWN(-1, false, false);
+    UNKNOWN(-1, false, false, "Unknown");
 
     private final int offset;
     private final boolean isGuild, isChannel;
+    private final String name;
 
-    Permission(int offset, boolean isGuild, boolean isChannel)
+    Permission(int offset, boolean isGuild, boolean isChannel, String name)
     {
         this.offset = offset;
         this.isGuild = isGuild;
         this.isChannel = isChannel;
+        this.name = name;
+    }
+    
+    /**
+     * The readable name as used in the Discord client.<br>
+     *
+     * @return
+     *      The readable name of this {@link net.dv8tion.jda.core.Permission Permission}.
+     */
+    public String getName() {
+        return this.name;
     }
 
     /**
