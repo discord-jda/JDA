9fe407944e3114a884ff726a69cef3272ef2f42d
Austin Keener <keeneraustin@yahoo.com>
2017-04-09 13:42:53 -0400

    Added code to example to handle webhook messages.

--- src/examples/java/MessageListenerExample.java
+++ src/examples/java/MessageListenerExample.java
@@ -42,7 +42,7 @@ public class MessageListenerExample extends ListenerAdapter
         {
             JDA jda = new JDABuilder(AccountType.BOT)
                     .setToken("Your-Token-Goes-Here")           //The token of the account that is logging in.
-                    .addListener(new MessageListenerExample())  //An instance of a class that will handle events.
+                    .addEventListener(new MessageListenerExample())  //An instance of a class that will handle events.
                     .buildBlocking();  //There are 2 ways to login, blocking vs async. Blocking guarantees that JDA will be completely loaded.
         }
         catch (LoginException e)
@@ -91,7 +91,7 @@ public class MessageListenerExample extends ListenerAdapter
         long responseNumber = event.getResponseNumber();//The amount of discord events that JDA has received since the last reconnect.
 
         //Event specific information
-        User author = event.getAuthor();                  //The user that sent the message
+        User author = event.getAuthor();                //The user that sent the message
         Message message = event.getMessage();           //The message that was received.
         MessageChannel channel = event.getChannel();    //This is the MessageChannel that the message was sent to.
                                                         //  This could be a TextChannel, PrivateChannel, or Group!
@@ -99,7 +99,7 @@ public class MessageListenerExample extends ListenerAdapter
         String msg = message.getContent();              //This returns a human readable version of the Message. Similar to
                                                         // what you would see in the client.
 
-        boolean bot = author.isBot();                     //This boolean is useful to determine if the User that
+        boolean bot = author.isBot();                    //This boolean is useful to determine if the User that
                                                         // sent the Message is a BOT or not!
 
         if (event.isFromType(ChannelType.TEXT))         //If this message was sent to a Guild TextChannel
@@ -112,8 +112,15 @@ public class MessageListenerExample extends ListenerAdapter
             TextChannel textChannel = event.getTextChannel(); //The TextChannel that this message was sent to.
             Member member = event.getMember();          //This Member that sent the message. Contains Guild specific information about the User!
 
-            String name = member.getEffectiveName();    //This will either use the Member's nickname if they have one,
-                                                        // otherwise it will default to their username. (User#getName())
+            String name;
+            if (message.isWebhookMessage())
+            {
+                name = author.getName();                //If this is a Webhook message, then there is no Member associated
+            }                                           // with the User, thus we default to the author for name.
+            else
+            {
+                name = member.getEffectiveName();       //This will either use the Member's nickname if they have one,
+            }                                           // otherwise it will default to their username. (User#getName())
 
             System.out.printf("(%s)[%s]<%s>: %s\n", guild.getName(), textChannel.getName(), name, msg);
         }
