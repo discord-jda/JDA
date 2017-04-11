f02b1e4f1d97fa40fdebae0bcad2d79464fe197a
Florian Spieß <business@minnced.club>
2017-04-11 06:05:40 +0200

    Added emote event classes (oops)

---
+++ src/main/java/net/dv8tion/jda/core/events/emote/GenericEmoteEvent.java
@@ -0,0 +1,49 @@
+/*
+ *     Copyright 2015-2017 Austin Keener & Michael Ritter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.dv8tion.jda.core.events.emote;
+
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.entities.Emote;
+import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.events.Event;
+
+public class GenericEmoteEvent extends Event
+{
+
+    protected final Emote emote;
+
+    public GenericEmoteEvent(JDA api, long responseNumber, Emote emote)
+    {
+        super(api, responseNumber);
+        this.emote = emote;
+    }
+
+    public Guild getGuild()
+    {
+        return emote.getGuild();
+    }
+
+    public Emote getEmote()
+    {
+        return emote;
+    }
+
+    public boolean isManaged()
+    {
+        return emote.isManaged();
+    }
+}
---
+++ src/main/java/net/dv8tion/jda/core/events/emote/update/EmoteUpdateRolesEvent.java
@@ -0,0 +1,47 @@
+/*
+ *     Copyright 2015-2017 Austin Keener & Michael Ritter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.dv8tion.jda.core.events.emote.update;
+
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.entities.Emote;
+import net.dv8tion.jda.core.entities.Role;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class EmoteUpdateRolesEvent extends GenericEmoteUpdateEvent
+{
+    protected final List<Role> oldRoles;
+
+    public EmoteUpdateRolesEvent(JDA api, long responseNumber, Emote emote, Collection<Role> oldRoles)
+    {
+        super(api, responseNumber, emote);
+        this.oldRoles = Collections.unmodifiableList(new LinkedList<>(oldRoles));
+    }
+
+    public List<Role> getOldRoles()
+    {
+        return oldRoles;
+    }
+
+    public List<Role> getNewRoles()
+    {
+        return emote.getRoles();
+    }
+}
