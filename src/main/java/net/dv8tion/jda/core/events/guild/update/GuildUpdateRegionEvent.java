0cac3a07c87e600ddcf20c52069cf02c3801d753
Florian Spieß <business@minnced.club>
2017-10-03 17:00:50 +0200

    Updated regions to use raw strings in Guild

--- src/main/java/net/dv8tion/jda/core/events/guild/update/GuildUpdateRegionEvent.java
+++ src/main/java/net/dv8tion/jda/core/events/guild/update/GuildUpdateRegionEvent.java
@@ -20,18 +20,71 @@ import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.Region;
 import net.dv8tion.jda.core.entities.Guild;
 
+/**
+ * <b><u>GuildUpdateRegionEvent</u></b>
+ * <br>Fired when the voice region of a {@link net.dv8tion.jda.core.entities.Guild Guild} has been
+ * updated.
+ * This can be used to retrieve the old and new region in either raw string names or resolved enum constants of {@link net.dv8tion.jda.core.Region Region}!
+ */
 public class GuildUpdateRegionEvent extends GenericGuildUpdateEvent
 {
-    private final Region oldRegion;
+    private final String oldRegion;
+    private final String newRegion;
 
-    public GuildUpdateRegionEvent(JDA api, long responseNumber, Guild guild, Region oldRegion)
+    public GuildUpdateRegionEvent(JDA api, long responseNumber, Guild guild, String oldRegion, String newRegion)
     {
         super(api, responseNumber, guild);
         this.oldRegion = oldRegion;
+        this.newRegion = newRegion;
     }
 
+    /**
+     * The old {@link net.dv8tion.jda.core.Region Region} of the {@link net.dv8tion.jda.core.entities.Guild Guild}.
+     * <br>If this region cannot be resolved to an enum constant this will return {@link net.dv8tion.jda.core.Region#UNKNOWN UNKNOWN}!
+     *
+     * <p>You can use {@link #getOldRegionRaw()} to get the raw name that discord provides for this region!
+     *
+     * @return Resolved {@link net.dv8tion.jda.core.Region Region} constant from the raw name
+     */
     public Region getOldRegion()
     {
+        return Region.fromKey(oldRegion);
+    }
+
+    /**
+     * The raw voice region name that was used prior to this update by the {@link net.dv8tion.jda.core.entities.Guild Guild}.
+     * <br>This can be resolved using {@link #getOldRegion()} to a constant of the enum. If that returns {@link net.dv8tion.jda.core.Region#UNKNOWN UNKNOWN}
+     * this region is not currently registered in JDA.
+     *
+     * @return Raw name of the old voice region
+     */
+    public String getOldRegionRaw()
+    {
         return oldRegion;
+    }
+
+    /**
+     * The new {@link net.dv8tion.jda.core.Region Region} of the {@link net.dv8tion.jda.core.entities.Guild Guild}.
+     * <br>If this region cannot be resolved to an enum constant this will return {@link net.dv8tion.jda.core.Region#UNKNOWN UNKNOWN}!
+     *
+     * <p>You can use {@link #getNewRegionRaw()} to get the raw name that discord provides for this region!
+     *
+     * @return Resolved {@link net.dv8tion.jda.core.Region Region} constant from the raw name
+     */
+    public Region getNewRegion()
+    {
+        return Region.fromKey(newRegion);
+    }
+
+    /**
+     * The raw voice region name that was updated to in the {@link net.dv8tion.jda.core.entities.Guild Guild}.
+     * <br>This can be resolved using {@link #getNewRegion()} to a constant of the enum. If that returns {@link net.dv8tion.jda.core.Region#UNKNOWN UNKNOWN}
+     * this region is not currently registered in JDA.
+     *
+     * @return Raw name of the updated voice region
+     */
+    public String getNewRegionRaw()
+    {
+        return newRegion;
     }
 }
