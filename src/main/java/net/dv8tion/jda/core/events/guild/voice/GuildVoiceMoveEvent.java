73706e7bdeb2b52d32973d81ea2473e081f5089c
MinnDevelopment <business@minnced.club>
2017-12-03 12:31:09 +0100

    Added GuildVoiceUpdateEvent to combine leave and move

--- src/main/java/net/dv8tion/jda/core/events/guild/voice/GuildVoiceMoveEvent.java
+++ src/main/java/net/dv8tion/jda/core/events/guild/voice/GuildVoiceMoveEvent.java
@@ -24,26 +24,14 @@ import net.dv8tion.jda.core.entities.VoiceChannel;
  * <b><u>GuildVoiceMoveEvent</u></b>
  * <p>Fired when a {@link net.dv8tion.jda.core.entities.Member Member} for any reason is moved from one {@link net.dv8tion.jda.core.entities.VoiceChannel VoiceChannel} to another
  */
-public class GuildVoiceMoveEvent extends GenericGuildVoiceEvent
+public class GuildVoiceMoveEvent extends GuildVoiceUpdateEvent
 {
-    protected final VoiceChannel channelLeft;
     protected final VoiceChannel channelJoined;
 
     public GuildVoiceMoveEvent(JDA api, long responseNumber, Member member, VoiceChannel channelLeft)
     {
-        super(api, responseNumber, member);
-        this.channelLeft = channelLeft;
+        super(api, responseNumber, member, channelLeft);
         this.channelJoined = member.getVoiceState().getChannel();
-    }
-
-    /**
-     * The {@link net.dv8tion.jda.core.entities.VoiceChannel VoiceChannel} that the {@link net.dv8tion.jda.core.entities.Member Member} is moved from
-     *
-     * @return the {@link net.dv8tion.jda.core.entities.VoiceChannel}
-     */
-    public VoiceChannel getChannelLeft()
-    {
-        return channelLeft;
     }
 
     /**
