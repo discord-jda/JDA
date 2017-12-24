d7876aac882f842d013d3782a0dc6ddaea8c6f45
TheMonitorLizard <kaidangustave@yahoo.com>
2017-12-23 23:22:35 -0600

    Made CategoryOrderAction extend ChannelOrderAction

--- src/main/java/net/dv8tion/jda/core/requests/restaction/order/CategoryOrderAction.java
+++ src/main/java/net/dv8tion/jda/core/requests/restaction/order/CategoryOrderAction.java
@@ -16,20 +16,14 @@
 
 package net.dv8tion.jda.core.requests.restaction.order;
 
-import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.entities.*;
-import net.dv8tion.jda.core.exceptions.InsufficientPermissionException;
-import net.dv8tion.jda.core.requests.Route;
 import net.dv8tion.jda.core.utils.Checks;
-import okhttp3.RequestBody;
-import org.json.JSONArray;
-import org.json.JSONObject;
+
 import java.util.List;
 
 /**
- * Implementation of {@link net.dv8tion.jda.core.requests.restaction.order.OrderAction OrderAction}
- * similar in functionality to {@link net.dv8tion.jda.core.requests.restaction.order.ChannelOrderAction ChannelOrderAction},
- * but constrained to the bounds of a single {@link net.dv8tion.jda.core.entities.Category Category}.
+ * An extension of {@link net.dv8tion.jda.core.requests.restaction.order.ChannelOrderAction ChannelOrderAction} with
+ * similar functionality, but constrained to the bounds of a single {@link net.dv8tion.jda.core.entities.Category Category}.
  * <br>To apply the changes you must finish the {@link net.dv8tion.jda.core.requests.RestAction RestAction}.
  *
  * <p>Before you can use any of the {@code move} methods
@@ -42,10 +36,9 @@ import java.util.List;
  *
  * @author Kaidan Gustave
  */
-public class CategoryOrderAction<T extends Channel> extends OrderAction<T, CategoryOrderAction<T>>
+public class CategoryOrderAction<T extends Channel> extends ChannelOrderAction<T>
 {
     protected final Category category;
-    protected final ChannelType type;
 
     /**
      * Creates a new CategoryOrderAction for the specified {@link net.dv8tion.jda.core.entities.Category Category}
@@ -64,39 +57,9 @@ public class CategoryOrderAction<T extends Channel> extends OrderAction<T, Categ
      */
     public CategoryOrderAction(Category category, ChannelType type)
     {
-        super(category.getJDA(), Route.Guilds.MODIFY_CHANNELS.compile(category.getGuild().getId()));
+        super(category.getGuild(), type,
+            (List<T>)(type == ChannelType.TEXT ? category.getTextChannels() : category.getVoiceChannels()));
         this.category = category;
-        this.type = type;
-
-        // Add all channels of a type in this Category
-        switch(type)
-        {
-            case TEXT:
-                this.orderList.addAll((List) category.getTextChannels());
-                break;
-            case VOICE:
-                this.orderList.addAll((List) category.getVoiceChannels());
-                break;
-            // Categories cannot currently be nested within categories so we just default here.
-            // If discord does allow more types of channels to be nested withing categories in
-            // the future, adding this functionality is just a matter of adding an additional case.
-            default:
-                throw new IllegalArgumentException("Cannot order channel type: "+type.name());
-        }
-    }
-
-    /**
-     * Gets the {@link net.dv8tion.jda.core.entities.Guild Guild} that
-     * the {@link net.dv8tion.jda.core.entities.Category Category}
-     * is from.
-     *
-     * @return The {@link net.dv8tion.jda.core.entities.Guild Guild} that
-     *         the {@link net.dv8tion.jda.core.entities.Category Category}
-     *         is from.
-     */
-    public Guild getGuild()
-    {
-        return getCategory().getGuild();
     }
 
     /**
@@ -109,38 +72,6 @@ public class CategoryOrderAction<T extends Channel> extends OrderAction<T, Categ
     public Category getCategory()
     {
         return category;
-    }
-
-    /**
-     * The {@link net.dv8tion.jda.core.entities.ChannelType ChannelType} of
-     * the channels that are ordered by this CategoryOrderAction.
-     *
-     * @return The {@link net.dv8tion.jda.core.entities.ChannelType ChannelType}
-     *         of the channels ordered by this CategoryOrderAction.
-     */
-    public ChannelType getChannelType()
-    {
-        return type;
-    }
-
-    @Override
-    protected RequestBody finalizeData()
-    {
-        final Member self = category.getGuild().getSelfMember();
-
-        // Make sure we have permission to actually do this
-        if(!self.hasPermission(category, Permission.MANAGE_CHANNEL))
-            throw new InsufficientPermissionException(Permission.MANAGE_CHANNEL);
-
-        JSONArray array = new JSONArray();
-        for(int i = 0; i < orderList.size(); i++)
-        {
-            JSONObject object = new JSONObject().put("id", orderList.get(i).getId())
-                                                .put("position", i);
-            array.put(object);
-        }
-
-        return getRequestBody(array);
     }
 
     @Override
