0b9d84c1bd0e481d686e983a485546d8e8c01e31
Florian Spieß <business@minnced.club>
2018-06-03 13:20:39 +0200

    Hardcode max snowflake length to 20 rather than calculate it
    The calculation using -1L requires the reader to understand
    two's complement while the hardcoded value is clear.

--- src/main/java/net/dv8tion/jda/core/utils/Checks.java
+++ src/main/java/net/dv8tion/jda/core/utils/Checks.java
@@ -20,8 +20,6 @@ import java.util.Collection;
 
 public class Checks
 {
-    private static final int maxSnowflakeLength = Long.toUnsignedString(-1L).length();
-
     public static void isSnowflake(final String snowflake)
     {
         isSnowflake(snowflake, snowflake);
@@ -30,7 +28,7 @@ public class Checks
     public static void isSnowflake(final String snowflake, final String message)
     {
         notNull(snowflake, message);
-        if (snowflake.length() < 1 || snowflake.length() > maxSnowflakeLength || !Helpers.isNumeric(snowflake))
+        if (snowflake.length() > 20 || !Helpers.isNumeric(snowflake))
             throw new IllegalArgumentException(message + " is not a valid snowflake value!");
     }
 
