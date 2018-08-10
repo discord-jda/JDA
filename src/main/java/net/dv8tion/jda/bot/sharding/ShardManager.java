6f298ef2c7577c376f67f4cc4155679cacb6bf37
Artuto <artutogamer@gmail.com>
2018-08-09 20:49:09 -0500

    removed unused import

--- src/main/java/net/dv8tion/jda/bot/sharding/ShardManager.java
+++ src/main/java/net/dv8tion/jda/bot/sharding/ShardManager.java
@@ -22,7 +22,6 @@ import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDA.Status;
 import net.dv8tion.jda.core.OnlineStatus;
 import net.dv8tion.jda.core.entities.*;
-import net.dv8tion.jda.core.exceptions.AccountTypeException;
 import net.dv8tion.jda.core.requests.Request;
 import net.dv8tion.jda.core.requests.Response;
 import net.dv8tion.jda.core.requests.RestAction;
