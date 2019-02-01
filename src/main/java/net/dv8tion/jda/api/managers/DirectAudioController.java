050f27ae012922e981e47a109ad64f85bfd36151
Florian Spieß <business@minnced.club>
2019-02-01 12:38:34 +0100

    Remove update from interface

--- src/main/java/net/dv8tion/jda/api/managers/DirectAudioController.java
+++ src/main/java/net/dv8tion/jda/api/managers/DirectAudioController.java
@@ -62,25 +62,4 @@ public interface DirectAudioController
      *        The channel we were connected to
      */
     void reconnect(VoiceChannel channel);
-
-    /**
-     * Used to update the internal state of the voice request. When a connection
-     * was successfully established JDA will stop sending requests for the initial connect.
-     * <br>This is done to retry the voice updates in case of a partial service failure.
-     *
-     * <p>Should be called when:
-     * <ol>
-     *     <li>Receiving a Voice State Update for the current account and we were previously connected (moved or disconnected)</li>
-     *     <li>Receiving a Voice Server Update (initial connect or region change)</li>
-     * </ol>
-     *
-     * Note that the voice state update will always be received prior to a voice server update.
-     * <br>The internal dispatch handlers already call this when needed, a library end-user never needs to call this method.
-     *
-     * @param guild
-     *        The guild to update the state for
-     * @param channel
-     *        The new channel, or null to signal disconnect
-     */
-    void update(Guild guild, VoiceChannel channel);
 }
--- src/main/java/net/dv8tion/jda/internal/managers/DirectAudioControllerImpl.java
+++ src/main/java/net/dv8tion/jda/internal/managers/DirectAudioControllerImpl.java
@@ -66,7 +66,25 @@ public class DirectAudioControllerImpl implements DirectAudioController
         client.queueAudioReconnect(channel);
     }
 
-    @Override
+    /**
+     * Used to update the internal state of the voice request. When a connection
+     * was successfully established JDA will stop sending requests for the initial connect.
+     * <br>This is done to retry the voice updates in case of a partial service failure.
+     *
+     * <p>Should be called when:
+     * <ol>
+     *     <li>Receiving a Voice State Update for the current account and we were previously connected (moved or disconnected)</li>
+     *     <li>Receiving a Voice Server Update (initial connect or region change)</li>
+     * </ol>
+     *
+     * Note that the voice state update will always be received prior to a voice server update.
+     * <br>The internal dispatch handlers already call this when needed, a library end-user never needs to call this method.
+     *
+     * @param guild
+     *        The guild to update the state for
+     * @param channel
+     *        The new channel, or null to signal disconnect
+     */
     public void update(Guild guild, VoiceChannel channel)
     {
         Checks.notNull(guild, "Guild");
