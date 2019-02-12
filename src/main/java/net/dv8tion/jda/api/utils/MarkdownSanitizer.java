5aac1c3189f0e1a481b19051ca9cec4821ccc1dd
Florian Spieß <business@minnced.club>
2019-02-13 00:43:51 +0100

    Handle escaped tokens

--- src/main/java/net/dv8tion/jda/api/utils/MarkdownSanitizer.java
+++ src/main/java/net/dv8tion/jda/api/utils/MarkdownSanitizer.java
@@ -32,6 +32,16 @@ public class MarkdownSanitizer
     public static final int UNDERLINE = 1 << 7; // __x__
     public static final int STRIKE =    1 << 8; // ~~x~~
 
+    public static final int ESCAPED_BOLD      = Integer.MIN_VALUE | BOLD;
+    public static final int ESCAPED_ITALICS_U = Integer.MIN_VALUE | ITALICS_U;
+    public static final int ESCAPED_ITALICS_A = Integer.MIN_VALUE | ITALICS_A;
+    public static final int ESCAPED_MONO      = Integer.MIN_VALUE | MONO;
+    public static final int ESCAPED_MONO_TWO  = Integer.MIN_VALUE | MONO_TWO;
+    public static final int ESCAPED_BLOCK     = Integer.MIN_VALUE | BLOCK;
+    public static final int ESCAPED_SPOILER   = Integer.MIN_VALUE | SPOILER;
+    public static final int ESCAPED_UNDERLINE = Integer.MIN_VALUE | UNDERLINE;
+    public static final int ESCAPED_STRIKE    = Integer.MIN_VALUE | STRIKE;
+
     private static final TIntObjectMap<String> tokens;
     static
     {
@@ -83,6 +93,14 @@ public class MarkdownSanitizer
 
     private int getRegion(int index, String sequence) //TODO: Handle escape?
     {
+        if (sequence.length() - index >= 4)
+        {
+            String fourChars = sequence.substring(index, index + 4);
+            if (fourChars.equals("\\```"))
+                return ESCAPED_BLOCK;
+            else if (fourChars.equals("\\***"))
+                return ESCAPED_BOLD | ITALICS_A;
+        }
         if (sequence.length() - index >= 3)
         {
             String threeChars = sequence.substring(index, index + 3);
@@ -92,6 +110,16 @@ public class MarkdownSanitizer
                     return BLOCK;
                 case "***":
                     return BOLD | ITALICS_A;
+                case "\\**":
+                    return ESCAPED_BOLD;
+                case "\\__":
+                    return ESCAPED_UNDERLINE;
+                case "\\~~":
+                    return ESCAPED_STRIKE;
+                case "\\``":
+                    return ESCAPED_MONO_TWO;
+                case "\\||":
+                    return ESCAPED_SPOILER;
             }
         }
         if (sequence.length() - index >= 2)
@@ -109,6 +137,12 @@ public class MarkdownSanitizer
                     return MONO_TWO;
                 case "||":
                     return SPOILER;
+                case "\\*":
+                    return ESCAPED_ITALICS_A;
+                case "\\_":
+                    return ESCAPED_ITALICS_U;
+                case "\\`":
+                    return ESCAPED_MONO;
             }
         }
         char current = sequence.charAt(index);
@@ -169,13 +203,23 @@ public class MarkdownSanitizer
     {
         switch (region)
         {
+            case ESCAPED_BLOCK:
+            case ESCAPED_BOLD | ITALICS_A:
+                return 4;
             case BLOCK:
             case BOLD | ITALICS_A:
+            case ESCAPED_MONO_TWO:
+            case ESCAPED_BOLD:
+            case ESCAPED_UNDERLINE:
+            case ESCAPED_SPOILER:
                 return 3;
             case MONO_TWO:
             case BOLD:
             case UNDERLINE:
             case SPOILER:
+            case ESCAPED_ITALICS_A:
+            case ESCAPED_ITALICS_U:
+            case ESCAPED_MONO:
                 return 2;
             case ITALICS_A:
             case ITALICS_U:
@@ -201,6 +245,11 @@ public class MarkdownSanitizer
                .append("\\").append(token);
     }
 
+    private boolean isEscape(int region)
+    {
+        return (Integer.MIN_VALUE & region) != 0;
+    }
+
     public String compute(String sequence)
     {
         StringBuilder builder = new StringBuilder();
@@ -214,7 +263,7 @@ public class MarkdownSanitizer
             }
 
             int endRegion = findEndIndex(i + 1, nextRegion, sequence);
-            if ((nextRegion & ignored) == nextRegion || endRegion == -1)
+            if (isEscape(nextRegion) || (nextRegion & ignored) == nextRegion || endRegion == -1)
             {
                 int delta = getDelta(nextRegion);
                 for (int j = 0; j < delta; j++)
