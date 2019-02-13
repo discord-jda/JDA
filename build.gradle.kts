d74e36a9eb5df918365c6f24b0beb0a601bbd480
Florian Spieß <business@minnced.club>
2019-02-13 01:32:19 +0100

    Add flag to fix javadoc

--- build.gradle.kts
+++ build.gradle.kts
@@ -200,6 +200,7 @@ javadoc.apply {
             "https://square.github.io/okhttp/3.x/okhttp/")
         if (JavaVersion.current().isJava9Compatible) {
             opt.addBooleanOption("html5", true)
+            opt.addBooleanOption("-no-module-directories", true)
         }
     }
 
