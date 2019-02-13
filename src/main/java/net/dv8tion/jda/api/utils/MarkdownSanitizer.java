3e3f29bdbc4732a0b0877b7021a08e3f7c374fa4
Florian Spieß <business@minnced.club>
2019-02-13 01:21:41 +0100

    Add missing documentation

--- src/main/java/net/dv8tion/jda/api/utils/MarkdownSanitizer.java
+++ src/main/java/net/dv8tion/jda/api/utils/MarkdownSanitizer.java
@@ -18,29 +18,45 @@ package net.dv8tion.jda.api.utils;
 
 import gnu.trove.map.TIntObjectMap;
 import gnu.trove.map.hash.TIntObjectHashMap;
+import net.dv8tion.jda.internal.utils.Checks;
 
+/**
+ * Implements and algorithm that can strip or replace markdown in any supplied string.
+ *
+ * @see #sanitize(String, net.dv8tion.jda.api.utils.MarkdownSanitizer.SanitizationStrategy)
+ */
 public class MarkdownSanitizer
 {
+    /** Normal characters that are not special for markdown, ignoring this has no effect */
     public static final int NORMAL =     0;
-    public static final int BOLD =      1 << 0; // **x**
+    /** Bold region such as "**Hello**" */
+    public static final int BOLD      = 1 << 0; // **x**
+    /** Italics region for underline such as "_Hello_" */
     public static final int ITALICS_U = 1 << 1; // _x_
+    /** Italics region for asterisks such as "*Hello*" */
     public static final int ITALICS_A = 1 << 2; // *x*
-    public static final int MONO =      1 << 3; // `x`
-    public static final int MONO_TWO =  1 << 4; // ``x``
-    public static final int BLOCK =     1 << 5; // ```x```
-    public static final int SPOILER =   1 << 6; // ||x||
+    /** Monospace region such as "`Hello`" */
+    public static final int MONO      = 1 << 3; // `x`
+    /** Monospace region such as "``Hello``" */
+    public static final int MONO_TWO  = 1 << 4; // ``x``
+    /** Codeblock region such as "```Hello```" */
+    public static final int BLOCK     = 1 << 5; // ```x```
+    /** Spoiler region such as "||Hello||" */
+    public static final int SPOILER   = 1 << 6; // ||x||
+    /** Underline region such as "__Hello__" */
     public static final int UNDERLINE = 1 << 7; // __x__
-    public static final int STRIKE =    1 << 8; // ~~x~~
+    /** Strikethrough region such as "~~Hello~~" */
+    public static final int STRIKE    = 1 << 8; // ~~x~~
 
-    public static final int ESCAPED_BOLD      = Integer.MIN_VALUE | BOLD;
-    public static final int ESCAPED_ITALICS_U = Integer.MIN_VALUE | ITALICS_U;
-    public static final int ESCAPED_ITALICS_A = Integer.MIN_VALUE | ITALICS_A;
-    public static final int ESCAPED_MONO      = Integer.MIN_VALUE | MONO;
-    public static final int ESCAPED_MONO_TWO  = Integer.MIN_VALUE | MONO_TWO;
-    public static final int ESCAPED_BLOCK     = Integer.MIN_VALUE | BLOCK;
-    public static final int ESCAPED_SPOILER   = Integer.MIN_VALUE | SPOILER;
-    public static final int ESCAPED_UNDERLINE = Integer.MIN_VALUE | UNDERLINE;
-    public static final int ESCAPED_STRIKE    = Integer.MIN_VALUE | STRIKE;
+    private static final int ESCAPED_BOLD      = Integer.MIN_VALUE | BOLD;
+    private static final int ESCAPED_ITALICS_U = Integer.MIN_VALUE | ITALICS_U;
+    private static final int ESCAPED_ITALICS_A = Integer.MIN_VALUE | ITALICS_A;
+    private static final int ESCAPED_MONO      = Integer.MIN_VALUE | MONO;
+    private static final int ESCAPED_MONO_TWO  = Integer.MIN_VALUE | MONO_TWO;
+    private static final int ESCAPED_BLOCK     = Integer.MIN_VALUE | BLOCK;
+    private static final int ESCAPED_SPOILER   = Integer.MIN_VALUE | SPOILER;
+    private static final int ESCAPED_UNDERLINE = Integer.MIN_VALUE | UNDERLINE;
+    private static final int ESCAPED_STRIKE    = Integer.MIN_VALUE | STRIKE;
 
     private static final TIntObjectMap<String> tokens;
     static
@@ -66,32 +82,80 @@ public class MarkdownSanitizer
     public MarkdownSanitizer(int ignored, SanitizationStrategy strategy)
     {
         this.ignored = ignored;
-        this.strategy = strategy;
+        this.strategy = strategy == null ? SanitizationStrategy.REMOVE : strategy;
     }
 
+    /**
+     * Sanitize string with default settings.
+     * <br>Same as {@code sanitize(sequence, SanitizationStrategy.REMOVE)}
+     *
+     * @param  sequence
+     *         The string to sanitize
+     *
+     * @return The sanitized string
+     */
     public static String sanitize(String sequence)
     {
         return sanitize(sequence, SanitizationStrategy.REMOVE);
     }
 
+    /**
+     * Sanitize string without ignoring anything.
+     *
+     * @param  sequence
+     *         The string to sanitize
+     * @param  strategy
+     *         The {@link net.dv8tion.jda.api.utils.MarkdownSanitizer.SanitizationStrategy} to apply
+     *
+     * @throws java.lang.IllegalArgumentException
+     *         If provided with null
+     *
+     * @return The sanitized string
+     *
+     * @see    MarkdownSanitizer#MarkdownSanitizer()
+     * @see    #withIgnored(int)
+     */
     public static String sanitize(String sequence, SanitizationStrategy strategy)
     {
+        Checks.notNull(sequence, "String");
+        Checks.notNull(strategy, "Strategy");
         return new MarkdownSanitizer().withStrategy(strategy).compute(sequence);
     }
 
+    /**
+     * Switches the used {@link net.dv8tion.jda.api.utils.MarkdownSanitizer.SanitizationStrategy}.
+     *
+     * @param  strategy
+     *         The new strategy
+     *
+     * @throws java.lang.IllegalArgumentException
+     *         If provided with null
+     *
+     * @return The current sanitizer instance with the new strategy
+     */
     public MarkdownSanitizer withStrategy(SanitizationStrategy strategy)
     {
+        Checks.notNull(strategy, "Strategy");
         this.strategy = strategy;
         return this;
     }
 
+    /**
+     * Specific regions to ignore.
+     * <br>Example: {@code new MarkdownSanitizer().withIgnored(MarkdownSanitizer.BOLD | MarkdownSanitizer.UNDERLINE).compute("Hello __world__!")}
+     *
+     * @param  ignored
+     *         The regions to ignore
+     *
+     * @return The current sanitizer instance with the new ignored regions
+     */
     public MarkdownSanitizer withIgnored(int ignored)
     {
         this.ignored |= ignored;
         return this;
     }
 
-    private int getRegion(int index, String sequence) //TODO: Handle escape?
+    private int getRegion(int index, String sequence)
     {
         if (sequence.length() - index >= 4)
         {
@@ -158,7 +222,7 @@ public class MarkdownSanitizer
         return NORMAL;
     }
 
-    public int findEndIndex(int afterIndex, int region, String sequence)
+    private int findEndIndex(int afterIndex, int region, String sequence)
     {
         if (isEscape(region))
             return -1;
@@ -275,8 +339,22 @@ public class MarkdownSanitizer
         return (nextRegion & ignored) == nextRegion;
     }
 
+    /**
+     * Computes the provided input.
+     * <br>Uses the specified {@link net.dv8tion.jda.api.utils.MarkdownSanitizer.SanitizationStrategy} and
+     * ignores any regions specified with {@link #withIgnored(int)}.
+     *
+     * @param  sequence
+     *         The string to compute
+     *
+     * @throws java.lang.IllegalArgumentException
+     *         If the provided string is null
+     *
+     * @return The resulting string after applying the computation
+     */
     public String compute(String sequence)
     {
+        Checks.notNull(sequence, "Input");
         StringBuilder builder = new StringBuilder();
         for (int i = 0; i < sequence.length();)
         {
@@ -304,7 +382,16 @@ public class MarkdownSanitizer
 
     public enum SanitizationStrategy
     {
+        /**
+         * Remove any format tokens that are not escaped or within a special region.
+         * <br>{@code "**Hello** World!" -> "Hello World!"}
+         */
         REMOVE,
+
+        /**
+         * Escape any format tokens that are not escaped or within a special region.
+         * <br>{@code "**Hello** World!" -> "\**Hello\** World!"}
+         */
         ESCAPE,
     }
 }
