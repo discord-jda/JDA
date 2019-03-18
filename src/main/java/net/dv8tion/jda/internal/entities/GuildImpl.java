d7406510e325d7a8eac5c192adf63e5bd55629a2
Florian Spieß <business@minnced.club>
2019-03-18 15:10:51 +0100

    Remove comment

--- src/main/java/net/dv8tion/jda/internal/entities/GuildImpl.java
+++ src/main/java/net/dv8tion/jda/internal/entities/GuildImpl.java
@@ -58,7 +58,7 @@ import java.util.concurrent.locks.ReentrantLock;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-public class GuildImpl implements Guild //TODO: Handle store channels
+public class GuildImpl implements Guild
 {
     private final long id;
     private final UpstreamReference<JDAImpl> api;
