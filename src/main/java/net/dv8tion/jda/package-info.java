00666009d6fe83eb069db6c8b2f7661df50a9ebc
Florian Spieß <business@minnced.club>
2019-06-28 17:59:26 +0200

    Fix typo
    Co-Authored-By: Austin Keener <keeneraustin@yahoo.com>

--- src/main/java/net/dv8tion/jda/package-info.java
+++ src/main/java/net/dv8tion/jda/package-info.java
@@ -19,7 +19,7 @@
  *
  * <p>From here you can navigate to the library features.
  * <ul>
- *     <li>{@link net.dv8tion.jda.api Api Features}
+ *     <li>{@link net.dv8tion.jda.api API Features}
  *     <br>Package which includes the core features of JDA, available throughout all feature packages</li>
  *
  *     <li>{@link net.dv8tion.jda.annotations Annotations}
