d72f1e975e2aa3dd4db0e1f290b86e2ff92157dc
Florian Spieß <business@minnced.club>
2019-12-29 09:11:42 +0100

    Double reconnect delay before connect
    Otherwise the delay won't increase unless the connect call fails

--- src/main/java/net/dv8tion/jda/internal/requests/WebSocketClient.java
+++ src/main/java/net/dv8tion/jda/internal/requests/WebSocketClient.java
@@ -538,6 +538,8 @@ public class WebSocketClient extends WebSocketAdapter implements WebSocketListen
         {
             api.setStatus(JDA.Status.WAITING_TO_RECONNECT);
             int delay = reconnectTimeoutS;
+            // Exponential backoff, reset on session creation (ready/resume)
+            reconnectTimeoutS = Math.min(reconnectTimeoutS << 1, api.getMaxReconnectDelay());
             Thread.sleep(delay * 1000);
             handleIdentifyRateLimit = false;
             api.setStatus(JDA.Status.ATTEMPTING_TO_RECONNECT);
@@ -556,7 +558,7 @@ public class WebSocketClient extends WebSocketAdapter implements WebSocketListen
             }
             catch (RuntimeException ex)
             {
-                reconnectTimeoutS = Math.min(reconnectTimeoutS << 1, api.getMaxReconnectDelay());
+                // reconnectTimeoutS = Math.min(reconnectTimeoutS << 1, api.getMaxReconnectDelay());
                 LOG.warn("Reconnect failed! Next attempt in {}s", reconnectTimeoutS);
             }
         }
