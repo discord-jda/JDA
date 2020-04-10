16237b911fba63e247da4783f300c5c528141409
Florian Spieß <business@minnced.club>
2020-04-10 19:28:54 +0200

    Add fallback operators onErrorMap and onErrorFlatMap to RestAction (#1219)

--- src/main/java/net/dv8tion/jda/internal/utils/Helpers.java
+++ src/main/java/net/dv8tion/jda/internal/utils/Helpers.java
@@ -142,4 +142,15 @@ public final class Helpers
         if (first == null || second == null) return false;
         return first.size() == second.size() && second.containsAll(first);
     }
+
+    // ## ExceptionUtils ##
+
+    public static <T extends Throwable> T appendCause(T throwable, Throwable cause)
+    {
+        Throwable t = throwable;
+        while (t.getCause() != null)
+            t = t.getCause();
+        t.initCause(cause);
+        return throwable;
+    }
 }
