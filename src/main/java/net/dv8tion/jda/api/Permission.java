35b0d4982e336576cd1900f8431c9dd8623a7484
Andre_601 <11576465+Andre601@users.noreply.github.com>
2020-04-21 18:58:11 +0200

    Remove invalid link in Permission enum

--- src/main/java/net/dv8tion/jda/api/Permission.java
+++ src/main/java/net/dv8tion/jda/api/Permission.java
@@ -137,8 +137,6 @@ public enum Permission
 
     /**
      * The binary offset of the permission.
-     * <br>For more information about Discord's offset system refer to
-     * <a href="https://discordapi.readthedocs.org/en/latest/reference/channels/permissions.html#permissions-number">Discord Permission Numbers</a>.
      *
      * @return The offset that represents this {@link net.dv8tion.jda.api.Permission Permission}.
      */
