b083ab3192513f02337e59ac850497e79e018e69
Florian Spieß <business@minnced.club>
2020-06-04 20:18:51 +0200

    Add support to DefaultShardManager

--- src/main/java/net/dv8tion/jda/api/sharding/DefaultShardManager.java
+++ src/main/java/net/dv8tion/jda/api/sharding/DefaultShardManager.java
@@ -16,7 +16,6 @@
 package net.dv8tion.jda.api.sharding;
 
 import gnu.trove.set.TIntSet;
-import net.dv8tion.jda.api.GatewayEncoding;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
@@ -592,7 +591,7 @@ public class DefaultShardManager implements ShardManager
         jda.setSelfUser(selfUser);
         jda.setStatus(JDA.Status.INITIALIZED); //This is already set by JDA internally, but this is to make sure the listeners catch it.
 
-        final int shardTotal = jda.login(this.gatewayURL, shardInfo, this.metaConfig.getCompression(), false, shardingConfig.getIntents(), GatewayEncoding.JSON);
+        final int shardTotal = jda.login(this.gatewayURL, shardInfo, this.metaConfig.getCompression(), false, shardingConfig.getIntents(), this.metaConfig.getEncoding());
         if (getShardsTotal() == -1)
             shardingConfig.setShardsTotal(shardTotal);
 
