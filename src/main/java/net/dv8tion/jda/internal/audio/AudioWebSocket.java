1126b705c6fd3c5fa9cc6527067664b4b960db15
Florian Spieß <business@minnced.club>
2020-07-21 02:27:38 +0200

    Update WebSocket library to 2.10
    This added a new copy constructor so we can cleanup some code

--- src/main/java/net/dv8tion/jda/internal/audio/AudioWebSocket.java
+++ src/main/java/net/dv8tion/jda/internal/audio/AudioWebSocket.java
@@ -120,18 +120,9 @@ class AudioWebSocket extends WebSocketAdapter
 
         try
         {
-            WebSocketFactory socketFactory = getJDA().getWebSocketFactory();
-            //noinspection SynchronizationOnLocalVariableOrMethodParameter
-            synchronized (socketFactory)
-            {
-                String host = IOUtil.getHost(wssEndpoint);
-                // null if the host is undefined, unlikely but we should handle it
-                if (host != null)
-                    socketFactory.setServerName(host);
-                else // practically should never happen
-                    socketFactory.setServerNames(null);
-                socket = socketFactory.createSocket(wssEndpoint);
-            }
+            WebSocketFactory socketFactory = new WebSocketFactory(getJDA().getWebSocketFactory());
+            IOUtil.setServerName(socketFactory, wssEndpoint);
+            socket = socketFactory.createSocket(wssEndpoint);
             socket.setDirectTextMessage(true);
             socket.addListener(this);
             changeStatus(ConnectionStatus.CONNECTING_AWAITING_WEBSOCKET_CONNECT);
