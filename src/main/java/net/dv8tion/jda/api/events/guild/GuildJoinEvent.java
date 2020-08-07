889375f30ebe2f8560ee5db77ea5a284eeffefeb
caneleex <git@mlnr.dev>
2020-08-07 21:25:57 +0200

    Fix typo in GuildJoinEvent docs

--- src/main/java/net/dv8tion/jda/api/events/guild/GuildJoinEvent.java
+++ src/main/java/net/dv8tion/jda/api/events/guild/GuildJoinEvent.java
@@ -22,7 +22,7 @@ import javax.annotation.Nonnull;
 
 /**
  * Indicates that you joined a {@link net.dv8tion.jda.api.entities.Guild Guild}.
- * <br>This requires that the guild is available when the guild leave happens. Otherwise a {@link UnavailableGuildJoinedEvent} is fired instead.
+ * <br>This requires that the guild is available when the guild join happens. Otherwise a {@link UnavailableGuildJoinedEvent} is fired instead.
  *
  * <p><b>Warning: Discord already triggered a mass amount of these events due to a downtime. Be careful!</b>
  *
