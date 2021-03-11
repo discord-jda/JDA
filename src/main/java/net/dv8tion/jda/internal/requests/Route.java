ec0437082233ac5787dd0ff5f10742253d2bac0c
Florian Spieß <business@minnced.club>
2021-03-11 16:20:51 +0100

    Remove /messages from execute endpoint

--- src/main/java/net/dv8tion/jda/internal/requests/Route.java
+++ src/main/java/net/dv8tion/jda/internal/requests/Route.java
@@ -70,7 +70,7 @@ public class Route
         public static final Route DELETE_GUILD_COMMAND = new Route(GET,  "applications/{application_id}/guilds/{guild_id}/commands/{command_id}");
 
         public static final Route CALLBACK =        new Route(POST,   "interactions/{interaction_id}/{interaction_token}/callback");
-        public static final Route CREATE_FOLLOWUP = new Route(POST,   "webhooks/{application_id}/{interaction_token}/messages");
+        public static final Route CREATE_FOLLOWUP = new Route(POST,   "webhooks/{application_id}/{interaction_token}");
         public static final Route EDIT_FOLLOWUP =   new Route(PATCH,  "webhooks/{application_id}/{interaction_token}/messages/{message_id}");
         public static final Route DELETE_FOLLOWUP = new Route(DELETE, "webhooks/{application_id}/{interaction_token}/messages/{message_id}");
     }
