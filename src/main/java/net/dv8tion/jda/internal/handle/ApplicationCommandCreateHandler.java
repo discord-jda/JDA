d0d0c36abcd23819737477ea656b02550adc29f1
Florian Spieß <business@minnced.club>
2021-03-15 21:37:07 +0100

    Fix ApplicationCommandCreateHandler

--- src/main/java/net/dv8tion/jda/internal/handle/ApplicationCommandCreateHandler.java
+++ src/main/java/net/dv8tion/jda/internal/handle/ApplicationCommandCreateHandler.java
@@ -18,6 +18,7 @@ package net.dv8tion.jda.internal.handle;
 
 import net.dv8tion.jda.api.entities.Command;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.events.application.ApplicationCommandCreateEvent;
 import net.dv8tion.jda.api.utils.data.DataObject;
 import net.dv8tion.jda.internal.JDAImpl;
 
@@ -44,6 +45,9 @@ public class ApplicationCommandCreateHandler extends SocketHandler
         }
 
         Command command = new Command(api, guild, content);
+        api.handleEvent(
+            new ApplicationCommandCreateEvent(api, responseNumber,
+                command, guild));
         return null;
     }
 }
