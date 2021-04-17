54841e8837234ddc756438f6540caca8891870cf
Daedalus <redstonedaedalus@gmail.com>
2021-04-17 13:15:45 -0700

    Deprecate UserFlag#SYSTEM and add User#isSystem (#1585)

--- src/main/java/net/dv8tion/jda/internal/entities/EntityBuilder.java
+++ src/main/java/net/dv8tion/jda/internal/entities/EntityBuilder.java
@@ -118,7 +118,8 @@ public class EntityBuilder
                 .setName(self.getString("username"))
                 .setDiscriminator(self.getString("discriminator"))
                 .setAvatarId(self.getString("avatar", null))
-                .setBot(self.getBoolean("bot"));
+                .setBot(self.getBoolean("bot"))
+                .setSystem(false);
 
         return selfUser;
     }
@@ -323,6 +324,7 @@ public class EntityBuilder
                    .setDiscriminator(user.get("discriminator").toString())
                    .setAvatarId(user.getString("avatar", null))
                    .setBot(user.getBoolean("bot"))
+                   .setSystem(user.getBoolean("system"))
                    .setFlags(user.getInt("public_flags", 0));
         }
         else
