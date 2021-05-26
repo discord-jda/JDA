fde34fcc8a70163fec5c3d3ad6e2010dea04e429
Florian Spieß <business@minnced.club>
2021-05-26 19:20:20 +0200

    Improve unicode checks

--- src/main/java/net/dv8tion/jda/internal/utils/Checks.java
+++ src/main/java/net/dv8tion/jda/internal/utils/Checks.java
@@ -24,8 +24,8 @@ import java.util.regex.Pattern;
 
 public class Checks
 {
-    public static final Pattern ALPHANUMERIC_WITH_DASH = Pattern.compile("[\\p{L}\\p{N}_-]+");
-    public static final Pattern ALPHANUMERIC = Pattern.compile("[\\p{L}\\p{N}_]+");
+    public static final Pattern ALPHANUMERIC_WITH_DASH = Pattern.compile("[\\w-]+");
+    public static final Pattern ALPHANUMERIC = Pattern.compile("[\\w]+", Pattern.UNICODE_CHARACTER_CLASS);
 
     @Contract("null -> fail")
     public static void isSnowflake(final String snowflake)
