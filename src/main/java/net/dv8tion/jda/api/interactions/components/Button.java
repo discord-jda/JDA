8eb37b28b4702f7a4dee32a1924f0f092c3b3e33
Florian Spieß <business@minnced.club>
2021-06-01 16:17:25 +0200

    Add missing parentheses

--- src/main/java/net/dv8tion/jda/api/interactions/components/Button.java
+++ src/main/java/net/dv8tion/jda/api/interactions/components/Button.java
@@ -38,13 +38,13 @@ import javax.annotation.Nullable;
  *           event.reply("Click the button to say hello")
  *               .addActionRow(
  *                 Button.primary("hello", "Click Me"), // Button with only a label
- *                 Button.success("emoji", Emoji.fromMarkdown("<:minn:245267426227388416>")) // Button with only an emoji
+ *                 Button.success("emoji", Emoji.fromMarkdown("<:minn:245267426227388416>"))) // Button with only an emoji
  *               .queue();
  *       } else if (event.getName().equals("info")) {
  *           event.reply("Click the buttons for more info")
  *               .addActionRow( // link buttons don't send events, they just open a link in the browser when clicked
  *                   Button.link("https://github.com/DV8FromTheWorld/JDA", "GitHub")
- *                     .withEmoji(Emoji.fromMarkdown("<:github:849286315580719104>"), // Link Button with label and emoji
+ *                     .withEmoji(Emoji.fromMarkdown("<:github:849286315580719104>")), // Link Button with label and emoji
  *                   Button.link("https://ci.dv8tion.net/job/JDA/javadoc/", "Javadocs")) // Link Button with only a label
  *               .queue();
  *       }
