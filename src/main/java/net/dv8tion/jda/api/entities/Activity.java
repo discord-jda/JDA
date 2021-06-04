d9fbbb5a4ab163818d443fc8e78e1f4cd38fea16
Andre_601 <11576465+Andre601@users.noreply.github.com>
2021-06-04 10:46:52 +0200

    Mention that CUSTOM_STATUS is read only (#1543)

--- src/main/java/net/dv8tion/jda/api/entities/Activity.java
+++ src/main/java/net/dv8tion/jda/api/entities/Activity.java
@@ -229,7 +229,7 @@ public interface Activity
     }
 
     /**
-     * Creates a new Activity instance with the specified name and url.
+     * Creates a new Activity instance with the specified name.
      *
      * @param  type
      *         The {@link net.dv8tion.jda.api.entities.Activity.ActivityType ActivityType} to use
@@ -237,9 +237,12 @@ public interface Activity
      *         The not-null name of the newly created game
      *
      * @throws IllegalArgumentException
-     *         If the specified name is null, empty or longer than 128 characters
+     *         <ul>
+     *           <li>If the specified ActivityType is null or unsupported</li>
+     *           <li>If the specified name is null, empty or longer than 128 characters</li>
+     *         </ul>
      *
-     * @return A valid Activity instance with the provided name and url
+     * @return A valid Activity instance with the provided name
      */
     @Nonnull
     static Activity of(@Nonnull ActivityType type, @Nonnull String name)
@@ -260,7 +263,10 @@ public interface Activity
      *         The streaming url to use, required to display as "streaming".
      *
      * @throws IllegalArgumentException
-     *         If the specified name is null, empty or longer than 128 characters
+     *         <ul>
+     *           <li>If the specified ActivityType is null or unsupported</li>
+     *           <li>If the specified name is null, empty or longer than 128 characters</li>
+     *         </ul>
      *
      * @return A valid Activity instance with the provided name and url
      *
@@ -331,7 +337,7 @@ public interface Activity
          * Used to indicate that the {@link Activity Activity} should display as a custom status
          * in the official client.
          *
-         * @incubating This feature is currently not officially documented and might change
+         * @incubating This Activity type is <b>read-only</b> for bots
          */
         @Incubating
         CUSTOM_STATUS(4),
