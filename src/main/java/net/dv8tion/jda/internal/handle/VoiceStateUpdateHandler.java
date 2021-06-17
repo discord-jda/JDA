d62eff4d713fb572cf1766daf85759e639f79813
Florian Spieß <business@minnced.club>
2021-06-17 09:22:13 +0200

    Add check for missing members in voice states

--- src/main/java/net/dv8tion/jda/internal/handle/VoiceStateUpdateHandler.java
+++ src/main/java/net/dv8tion/jda/internal/handle/VoiceStateUpdateHandler.java
@@ -26,6 +26,7 @@ import net.dv8tion.jda.internal.entities.GuildVoiceStateImpl;
 import net.dv8tion.jda.internal.entities.MemberImpl;
 import net.dv8tion.jda.internal.entities.VoiceChannelImpl;
 import net.dv8tion.jda.internal.managers.AudioManagerImpl;
+import net.dv8tion.jda.internal.requests.WebSocketClient;
 
 import java.time.OffsetDateTime;
 import java.util.Objects;
@@ -45,6 +46,14 @@ public class VoiceStateUpdateHandler extends SocketHandler
             return null; //unhandled for calls
         if (getJDA().getGuildSetupController().isLocked(guildId))
             return guildId;
+
+        // TODO: Handle these voice states properly
+        if (content.isNull("member"))
+        {
+            WebSocketClient.LOG.debug("Discarding VOICE_STATE_UPDATE with missing member. JSON: {}", content);
+            return null;
+        }
+
         handleGuildVoiceState(content);
         return null;
     }
