f85a820d4c7e84bdadd9ea25f6bb43bd5cf6bf28
VixenKasai <62079191+VixenKasai@users.noreply.github.com>
2021-06-27 15:50:52 -0500

    Add SlashCommandEvent.getCommandString
    (Updated Files from last PR, branch corrupted)

--- src/main/java/net/dv8tion/jda/api/interactions/commands/CommandInteraction.java
+++ src/main/java/net/dv8tion/jda/api/interactions/commands/CommandInteraction.java
@@ -16,6 +16,8 @@
 
 package net.dv8tion.jda.api.interactions.commands;
 
+import net.dv8tion.jda.api.entities.Emote;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.interactions.Interaction;
 import net.dv8tion.jda.internal.utils.Checks;
@@ -199,5 +201,60 @@ public interface CommandInteraction extends Interaction
     {
         List<OptionMapping> options = getOptionsByName(name);
         return options.isEmpty() ? null : options.get(0);
+    }
+
+    /**
+     * Gets the slash command String for this slash command.
+     * <br>This is similar to the String you see when clicking the interaction name in the client.
+     *
+     * <p>Example return for an echo command: {@code /say echo phrase: Say this}
+     *
+     * @return The command String for this slash command
+     */
+    @Nonnull
+    default String getCommandString()
+    {
+        //Get text like the text that appears when you hover over the interaction in discord
+        StringBuilder builder = new StringBuilder();
+        builder.append("/").append(getName());
+        if (getSubcommandGroup() != null)
+            builder.append(getSubcommandGroup()).append(" ");
+        if (getSubcommandName() != null)
+            builder.append(getSubcommandName()).append(" ");
+        builder.append(" ");
+        //build options (formatted appropriately)
+        for (OptionMapping o : getOptions())
+        {
+            builder.append(o.getName()).append(": ");
+            switch (o.getType())
+            {
+            case CHANNEL:
+                builder.append("#").append(o.getAsGuildChannel().getName()).append(" ");
+                break;
+            case USER:
+                builder.append("@").append(o.getAsUser().getName()).append(" ");
+                break;
+            case ROLE:
+                builder.append("@").append(o.getAsRole().getName()).append(" ");
+                break;
+            case MENTIONABLE:
+                if (o instanceof Member)
+                {
+                    builder.append("@").append(
+                           ((Member) o).getEffectiveName()
+                    ).append(" ");
+                }
+                else if (o instanceof Emote)
+                {
+                    builder.append("@").append(
+                            ((Emote) o).getName()
+                    ).append(" ");
+                }
+            default:
+                builder.append(o.getAsString()).append(" ");
+                break;
+            }
+        }
+        return builder.toString().trim();
     }
 }
