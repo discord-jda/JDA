3c0984c3440e59d80663cc24d393e3dd65087fc9
Florian Spieß <business@minnced.club>
2021-06-27 14:35:00 +0200

    Add documentation

--- src/main/java/net/dv8tion/jda/api/utils/Timestamp.java
+++ src/main/java/net/dv8tion/jda/api/utils/Timestamp.java
@@ -22,41 +22,88 @@ import javax.annotation.Nonnull;
 import java.time.Duration;
 import java.time.Instant;
 
+/**
+ * Utility class representing Discord Markdown timestamps.
+ * <br>This class implements {@link #toString()} such that it can be directly included in message content.
+ *
+ * <p>These timestamps are rendered by the individual receiving discord client in a local timezone and language format.
+ * Each timestamp can be displayed with different {@link TimeFormat TimeFormats}.
+ */
 public class Timestamp
 {
     private final TimeFormat format;
     private final long timestamp;
 
-    public Timestamp(TimeFormat format, long timestamp)
+    protected Timestamp(TimeFormat format, long timestamp)
     {
         Checks.notNull(format, "TimeFormat");
         this.format = format;
         this.timestamp = timestamp;
     }
 
+    /**
+     * The {@link TimeFormat} used to display this timestamp.
+     *
+     * @return The {@link TimeFormat}
+     */
     @Nonnull
     public TimeFormat getFormat()
     {
         return format;
     }
 
+    /**
+     * The unix epoch timestamp for this markdown timestamp.
+     * <br>This is similar to {@link System#currentTimeMillis()} and provided in millisecond precision for easier compatibility.
+     * Discord uses seconds precision instead.
+     *
+     * @return The millisecond unix epoch timestamp
+     */
     public long getTimestamp()
     {
         return timestamp;
     }
 
+    /**
+     * Shortcut for {@code Instant.ofEpochMilli(getTimestamp())}.
+     *
+     * @return The {@link Instant} of this timestamp
+     */
     @Nonnull
     public Instant toInstant()
     {
         return Instant.ofEpochMilli(timestamp);
     }
 
+    /**
+     * Creates a new timestamp instance with the provided offset into the future relative to the current timestamp.
+     *
+     * @param  millis
+     *         The millisecond offset for the new timestamp
+     *
+     * @return Copy of this timestamp with the relative offset
+     *
+     * @see    #plus(Duration)
+     */
     @Nonnull
     public Timestamp plus(long millis)
     {
         return new Timestamp(format, timestamp + millis);
     }
 
+    /**
+     * Creates a new timestamp instance with the provided offset into the future relative to the current timestamp.
+     *
+     * @param  duration
+     *         The offset for the new timestamp
+     *
+     * @throws IllegalArgumentException
+     *         If the provided duration is null
+     *
+     * @return Copy of this timestamp with the relative offset
+     *
+     * @see    #plus(long)
+     */
     @Nonnull
     public Timestamp plus(@Nonnull Duration duration)
     {
@@ -64,12 +111,35 @@ public class Timestamp
         return plus(duration.toMillis());
     }
 
+    /**
+     * Creates a new timestamp instance with the provided offset into the past relative to the current timestamp.
+     *
+     * @param  millis
+     *         The millisecond offset for the new timestamp
+     *
+     * @return Copy of this timestamp with the relative offset
+     *
+     * @see    #minus(Duration)
+     */
     @Nonnull
     public Timestamp minus(long millis)
     {
         return new Timestamp(format, timestamp - millis);
     }
 
+    /**
+     * Creates a new timestamp instance with the provided offset into the past relative to the current timestamp.
+     *
+     * @param  duration
+     *         The offset for the new timestamp
+     *
+     * @throws IllegalArgumentException
+     *         If the provided duration is null
+     *
+     * @return Copy of this timestamp with the relative offset
+     *
+     * @see    #minus(long)
+     */
     @Nonnull
     public Timestamp minus(@Nonnull Duration duration)
     {
@@ -80,6 +150,6 @@ public class Timestamp
     @Override
     public String toString()
     {
-        return "<t:" + timestamp / 1000 + ":" + format.getFlag() + ">";
+        return "<t:" + timestamp / 1000 + ":" + format.getStyle() + ">";
     }
 }
