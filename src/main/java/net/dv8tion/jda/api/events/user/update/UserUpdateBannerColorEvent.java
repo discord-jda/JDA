a9542d75159eb64211951fabd64f3f3c69055c4b
RedDaedalus <redstonedaedalus@gmail.com>
2021-07-19 15:36:48 -0600

    Event docs

--- src/main/java/net/dv8tion/jda/api/events/user/update/UserUpdateBannerColorEvent.java
+++ src/main/java/net/dv8tion/jda/api/events/user/update/UserUpdateBannerColorEvent.java
@@ -7,6 +7,18 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.awt.*;
 
+/**
+ * Indicates that the banner color of a {@link net.dv8tion.jda.api.entities.User User} changed.
+ *
+ * <p>Can be used to retrieve the user who changed their banner color and their previous banner color.
+ *
+ * <p>Identifier: {@code banner_color}
+ *
+ * <h2>Requirements</h2>
+ * <p>This event requires that the user be fetched by the {@link net.dv8tion.jda.api.JDA#retrieveUserById(long)} method
+ * as the user's profile data is not sent directly. Thus, the old value will be null even if the user has a banner color
+ * unless previously fetched.
+ */
 public class UserUpdateBannerColorEvent extends GenericUserUpdateEvent<Color>
 {
     public static final String IDENTIFIER = "banner_color";
@@ -16,11 +28,21 @@ public class UserUpdateBannerColorEvent extends GenericUserUpdateEvent<Color>
         super(api, responseNumber, user, oldValue, user.getBannerColor(), IDENTIFIER);
     }
 
+    /**
+     * The old banner color
+     *
+     * @return The old banner color, null if previously unknown or unset.
+     */
     public Color getOldBannerColor()
     {
         return getOldValue();
     }
 
+    /**
+     * The new banner color
+     *
+     * @return The new banner color
+     */
     public Color getNewBannerColor()
     {
         return getNewValue();
--- src/main/java/net/dv8tion/jda/api/events/user/update/UserUpdateBannerEvent.java
+++ src/main/java/net/dv8tion/jda/api/events/user/update/UserUpdateBannerEvent.java
@@ -6,6 +6,18 @@ import net.dv8tion.jda.api.entities.User;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
+/**
+ * Indicates that the banner of a {@link net.dv8tion.jda.api.entities.User User} changed.
+ *
+ * <p>Can be used to retrieve the user who changed their banner and their previous banner.
+ *
+ * <p>Identifier: {@code banner}
+ *
+ * <h2>Requirements</h2>
+ * <p>This event requires that the user be fetched by the {@link net.dv8tion.jda.api.JDA#retrieveUserById(long)} method
+ * as the user's profile data is not sent directly. Thus, the old value will be null even if the user has a banner unless
+ * previously fetched.
+ */
 public class UserUpdateBannerEvent extends GenericUserUpdateEvent<String>
 {
     public static final String IDENTIFIER = "user_banner";
@@ -15,24 +27,44 @@ public class UserUpdateBannerEvent extends GenericUserUpdateEvent<String>
         super(api, responseNumber, user, oldBanner, user.getBannerId(), IDENTIFIER);
     }
 
+    /**
+     * The previous banner id
+     *
+     * @return The previous banner id, null if previously unknown or unset.
+     */
     @Nullable
     public String getOldBannerId()
     {
         return getOldValue();
     }
 
+    /**
+     * The previous banner url
+     *
+     * @return The previous banner url, null if previously unknown or unset.
+     */
     @Nullable
     public String getOldBannerUrl()
     {
         return previous == null ? null : String.format(User.BANNER_URL, getUser().getId(), previous, previous.startsWith("a_") ? "gif" : "png");
     }
 
+    /**
+     * The new banner id
+     *
+     * @return The new banner id
+     */
     @Nullable
     public String getNewBannerId()
     {
         return getNewValue();
     }
 
+    /**
+     * The new banner url
+     *
+     * @return The new banner url
+     */
     @Nullable
     public String getNewBannerUrl()
     {
