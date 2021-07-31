335394ff01e9d136e506750c2b0881c5ee573deb
aryn <aryn.folkard@null.net>
2021-07-31 20:19:50 +0100

    Restore Changes

--- src/main/java/net/dv8tion/jda/api/EmbedBuilder.java
+++ src/main/java/net/dv8tion/jda/api/EmbedBuilder.java
@@ -156,7 +156,7 @@ public class EmbedBuilder
      */
     public boolean isEmpty()
     {
-        return title == null
+        return (title == null || title.trim().isEmpty())
             && timestamp == null
             && thumbnail == null
             && author == null
@@ -175,7 +175,7 @@ public class EmbedBuilder
      */
     public int length()
     {
-        int length = description.length();
+        int length = description.toString().trim().length();
         synchronized (fields)
         {
             length = fields.stream().map(f -> f.getName().length() + f.getValue().length()).reduce(length, Integer::sum);
