571c292f8c5926767a2f1712303d8da81d7ff8e3
IAmSpecial <48771274+IAmSpecial@users.noreply.github.com>
2021-08-01 15:09:33 -0400

    Correctly add getData()

--- src/main/java/net/dv8tion/jda/internal/interactions/CommandInteractionImpl.java
+++ src/main/java/net/dv8tion/jda/internal/interactions/CommandInteractionImpl.java
@@ -30,6 +30,7 @@ import net.dv8tion.jda.internal.entities.EntityBuilder;
 import net.dv8tion.jda.internal.entities.GuildImpl;
 import net.dv8tion.jda.internal.entities.MemberImpl;
 import net.dv8tion.jda.internal.entities.UserImpl;
+import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nonnull;
 import java.util.ArrayList;
@@ -44,6 +45,7 @@ public class CommandInteractionImpl extends InteractionImpl implements CommandIn
     private final String name;
     private String subcommand;
     private String group;
+    private final DataObject data;
 
     public CommandInteractionImpl(JDAImpl jda, DataObject data)
     {
@@ -51,6 +53,7 @@ public class CommandInteractionImpl extends InteractionImpl implements CommandIn
         DataObject commandData = data.getObject("data");
         this.commandId = commandData.getUnsignedLong("id");
         this.name = commandData.getString("name");
+        this.data = data;
 
         DataArray options = commandData.optArray("options").orElseGet(DataArray::empty);
         DataObject resolveJson = commandData.optObject("resolved").orElseGet(DataObject::empty);
@@ -161,5 +164,12 @@ public class CommandInteractionImpl extends InteractionImpl implements CommandIn
     public List<OptionMapping> getOptions()
     {
         return options;
+    }
+
+    @NotNull
+    @Override
+    public DataObject getData()
+    {
+        return data;
     }
 }
