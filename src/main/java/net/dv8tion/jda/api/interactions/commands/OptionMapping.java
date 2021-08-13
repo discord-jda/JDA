ecee993a158bf775528aab8ff6c3dc6f81b1ff90
Austin Keener <keeneraustin@yahoo.com>
2021-08-13 12:53:39 -0400

    Renamed AbstractChannel -> Channel interface

--- src/main/java/net/dv8tion/jda/api/interactions/commands/OptionMapping.java
+++ src/main/java/net/dv8tion/jda/api/interactions/commands/OptionMapping.java
@@ -215,7 +215,7 @@ public class OptionMapping
     @Nonnull
     public GuildChannel getAsGuildChannel()
     {
-        AbstractChannel value = getAsChannel();
+        Channel value = getAsChannel();
         if (value instanceof GuildChannel)
             return (GuildChannel) value;
         throw new IllegalStateException("Could not resolve GuildChannel!");
@@ -233,7 +233,7 @@ public class OptionMapping
     @Nullable
     public MessageChannel getAsMessageChannel()
     {
-        AbstractChannel value = getAsChannel();
+        Channel value = getAsChannel();
         return value instanceof MessageChannel ? (MessageChannel) value : null;
     }
 
@@ -248,7 +248,7 @@ public class OptionMapping
     @Nonnull
     public ChannelType getChannelType()
     {
-        AbstractChannel channel = getAsChannel();
+        Channel channel = getAsChannel();
         return channel == null ? ChannelType.UNKNOWN : channel.getType();
     }
 
@@ -276,10 +276,10 @@ public class OptionMapping
     }
 
     @Nullable
-    private AbstractChannel getAsChannel()
+    private Channel getAsChannel()
     {
         if (type != OptionType.CHANNEL)
             throw new IllegalStateException("Cannot resolve AbstractChannel for option " + getName() + " of type " + type);
-        return (AbstractChannel) resolved.get(getAsLong());
+        return (Channel) resolved.get(getAsLong());
     }
 }
