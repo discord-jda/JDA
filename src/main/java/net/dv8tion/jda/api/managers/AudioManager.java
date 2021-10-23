0ff7693b3c7a4ee533c2cb0ffe893febb62ea887
Austin Keener <keeneraustin@yahoo.com>
2021-10-22 21:13:11 -0400

    Updating development with docs changes (#1867)
    Co-authored-by: aryn <aryn@no-doxxing-allowed.com>
    Co-authored-by: Austin Keener <keeneraustin@yahoo.com>
    Co-authored-by: Tais993 <49957334+Tais993@users.noreply.github.com>
    Co-authored-by: arynxd <64087640+arynxd@users.noreply.github.com>
    Co-authored-by: Andre_601 <11576465+Andre601@users.noreply.github.com>
    Co-authored-by: Marko Zajc <marko@zajc.eu.org>
    Co-authored-by: caneleex <git@mlnr.dev>
    Co-authored-by: Alf <62615304+Alf-Melmac@users.noreply.github.com>
    Co-authored-by: PASTOR CHarles <40164679+stormer3428@users.noreply.github.com>

--- src/main/java/net/dv8tion/jda/api/managers/AudioManager.java
+++ src/main/java/net/dv8tion/jda/api/managers/AudioManager.java
@@ -189,7 +189,7 @@ public interface AudioManager
      * @deprecated The internals have changed and this is no longer used
      */
     @Deprecated
-    @ForRemoval
+    @ForRemoval(deadline = "5.0.0")
     @DeprecatedSince("4.2.0")
     boolean isAttemptingToConnect();
 
@@ -207,7 +207,7 @@ public interface AudioManager
      */
     @Nullable
     @Deprecated
-    @ForRemoval
+    @ForRemoval(deadline = "5.0.0")
     @DeprecatedSince("4.2.0")
     VoiceChannel getQueuedAudioConnection();
 
