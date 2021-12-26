9aef033d9385f41c8dd12e27264fcc0bfd66bec6
Florian Spieß <business@minnced.club>
2021-12-26 17:58:52 +0100

    Add docs for auto-complete callbacks

--- src/main/java/net/dv8tion/jda/api/requests/restaction/interactions/AutoCompleteCallbackAction.java
+++ src/main/java/net/dv8tion/jda/api/requests/restaction/interactions/AutoCompleteCallbackAction.java
@@ -18,6 +18,7 @@ package net.dv8tion.jda.api.requests.restaction.interactions;
 
 import net.dv8tion.jda.api.interactions.commands.Command;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.dv8tion.jda.internal.utils.Checks;
 
 import javax.annotation.CheckReturnValue;
@@ -26,15 +27,63 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
+/**
+ * An {@link InteractionCallbackAction} that can be used to suggest auto-complete choices.
+ */
 public interface AutoCompleteCallbackAction extends InteractionCallbackAction<Void>
 {
+    /**
+     * The {@link OptionType} of the choices you can suggest.
+     *
+     * @return The option type
+     */
     @Nonnull
     OptionType getOptionType();
 
+    /**
+     * Add up to {@value OptionData#MAX_CHOICES} choices which can be picked from by the user.
+     * <br>The user may continue writing inputs instead of using one of your choices.
+     *
+     * @param  choices
+     *         The choice suggestions to present to the user
+     *
+     * @throws IllegalArgumentException
+     *         If any of the following is true:
+     *         <ul>
+     *             <li>{@code null} is provided</li>
+     *             <li>More than {@value OptionData#MAX_CHOICES} are added</li>
+     *             <li>Any of the choice names is empty or longer than {@value OptionData#MAX_CHOICE_NAME_LENGTH}</li>
+     *             <li>The option type is incompatible with the choice type</li>
+     *             <li>The numeric value of any of the choices is not between {@value  OptionData#MIN_NEGATIVE_NUMBER} and {@value OptionData#MAX_POSITIVE_NUMBER}</li>
+     *             <li>The string value of any of the choices is empty or longer than {@value OptionData#MAX_CHOICE_VALUE_LENGTH}</li>
+     *         </ul>
+     *
+     * @return The same callback action, for chaining convenience
+     */
     @Nonnull
     @CheckReturnValue
     AutoCompleteCallbackAction addChoices(@Nonnull Collection<Command.Choice> choices);
 
+    /**
+     * Add up to {@value OptionData#MAX_CHOICES} choices which can be picked from by the user.
+     * <br>The user may continue writing inputs instead of using one of your choices.
+     *
+     * @param  choices
+     *         The choice suggestions to present to the user
+     *
+     * @throws IllegalArgumentException
+     *         If any of the following is true:
+     *         <ul>
+     *             <li>{@code null} is provided</li>
+     *             <li>More than {@value OptionData#MAX_CHOICES} are added</li>
+     *             <li>Any of the choice names is empty or longer than {@value OptionData#MAX_CHOICE_NAME_LENGTH}</li>
+     *             <li>The option type is incompatible with the choice type</li>
+     *             <li>The numeric value of any of the choices is not between {@value  OptionData#MIN_NEGATIVE_NUMBER} and {@value OptionData#MAX_POSITIVE_NUMBER}</li>
+     *             <li>The string value of any of the choices is empty or longer than {@value OptionData#MAX_CHOICE_VALUE_LENGTH}</li>
+     *         </ul>
+     *
+     * @return The same callback action, for chaining convenience
+     */
     @Nonnull
     @CheckReturnValue
     default AutoCompleteCallbackAction addChoices(@Nonnull Command.Choice... choices)
@@ -43,6 +92,27 @@ public interface AutoCompleteCallbackAction extends InteractionCallbackAction<Vo
         return addChoices(Arrays.asList(choices));
     }
 
+    /**
+     * Add up to {@value OptionData#MAX_CHOICES} choices which can be picked from by the user.
+     * <br>The user may continue writing inputs instead of using one of your choices.
+     *
+     * @param  name
+     *         The choice name to show to the user, 1-{@value OptionData#MAX_CHOICE_NAME_LENGTH} characters
+     * @param  value
+     *         The choice value, 1-{@value OptionData#MAX_CHOICE_VALUE_LENGTH} characters
+     *
+     * @throws IllegalArgumentException
+     *         If any of the following is true:
+     *         <ul>
+     *             <li>{@code null} is provided</li>
+     *             <li>More than {@value OptionData#MAX_CHOICES} are added</li>
+     *             <li>The choice name is empty or longer than {@value OptionData#MAX_CHOICE_NAME_LENGTH}</li>
+     *             <li>The option type is not {@link OptionType#STRING}</li>
+     *             <li>The string value of any of the choices is empty or longer than {@value OptionData#MAX_CHOICE_VALUE_LENGTH}</li>
+     *         </ul>
+     *
+     * @return The same callback action, for chaining convenience
+     */
     @Nonnull
     @CheckReturnValue
     default AutoCompleteCallbackAction addChoice(@Nonnull String name, @Nonnull String value)
@@ -50,6 +120,27 @@ public interface AutoCompleteCallbackAction extends InteractionCallbackAction<Vo
         return addChoices(new Command.Choice(name, value));
     }
 
+    /**
+     * Add up to {@value OptionData#MAX_CHOICES} choices which can be picked from by the user.
+     * <br>The user may continue writing inputs instead of using one of your choices.
+     *
+     * @param  name
+     *         The choice name to show to the user, 1-{@value OptionData#MAX_CHOICE_NAME_LENGTH} characters
+     * @param  value
+     *         The choice value, must be between {@value  OptionData#MIN_NEGATIVE_NUMBER} and {@value OptionData#MAX_POSITIVE_NUMBER}
+     *
+     * @throws IllegalArgumentException
+     *         If any of the following is true:
+     *         <ul>
+     *             <li>{@code null} is provided</li>
+     *             <li>More than {@value OptionData#MAX_CHOICES} are added</li>
+     *             <li>The choice name is empty or longer than {@value OptionData#MAX_CHOICE_NAME_LENGTH}</li>
+     *             <li>The option type is incompatible with the choice type</li>
+     *             <li>The value of is not between {@value  OptionData#MIN_NEGATIVE_NUMBER} and {@value OptionData#MAX_POSITIVE_NUMBER}</li>
+     *         </ul>
+     *
+     * @return The same callback action, for chaining convenience
+     */
     @Nonnull
     @CheckReturnValue
     default AutoCompleteCallbackAction addChoice(@Nonnull String name, long value)
@@ -57,6 +148,27 @@ public interface AutoCompleteCallbackAction extends InteractionCallbackAction<Vo
         return addChoices(new Command.Choice(name, value));
     }
 
+    /**
+     * Add up to {@value OptionData#MAX_CHOICES} choices which can be picked from by the user.
+     * <br>The user may continue writing inputs instead of using one of your choices.
+     *
+     * @param  name
+     *         The choice name to show to the user, 1-{@value OptionData#MAX_CHOICE_NAME_LENGTH} characters
+     * @param  value
+     *         The choice value, must be between {@value  OptionData#MIN_NEGATIVE_NUMBER} and {@value OptionData#MAX_POSITIVE_NUMBER}
+     *
+     * @throws IllegalArgumentException
+     *         If any of the following is true:
+     *         <ul>
+     *             <li>{@code null} is provided</li>
+     *             <li>More than {@value OptionData#MAX_CHOICES} are added</li>
+     *             <li>The choice name is empty or longer than {@value OptionData#MAX_CHOICE_NAME_LENGTH}</li>
+     *             <li>The option type is incompatible with the choice type</li>
+     *             <li>The value of is not between {@value  OptionData#MIN_NEGATIVE_NUMBER} and {@value OptionData#MAX_POSITIVE_NUMBER}</li>
+     *         </ul>
+     *
+     * @return The same callback action, for chaining convenience
+     */
     @Nonnull
     @CheckReturnValue
     default AutoCompleteCallbackAction addChoice(@Nonnull String name, double value)
@@ -64,6 +176,26 @@ public interface AutoCompleteCallbackAction extends InteractionCallbackAction<Vo
         return addChoices(new Command.Choice(name, value));
     }
 
+    /**
+     * Add up to {@value OptionData#MAX_CHOICES} choices which can be picked from by the user.
+     * <br>The user may continue writing inputs instead of using one of your choices.
+     *
+     * <p>The provided strings will be used as value and name for the {@link net.dv8tion.jda.api.interactions.commands.Command.Choice Choices}.
+     *
+     * @param  choices
+     *         The choice suggestions to present to the user, each limited to {@value OptionData#MAX_CHOICE_NAME_LENGTH} characters
+     *
+     * @throws IllegalArgumentException
+     *         If any of the following is true:
+     *         <ul>
+     *             <li>{@code null} is provided</li>
+     *             <li>More than {@value OptionData#MAX_CHOICES} are added</li>
+     *             <li>Any of the choice names is empty or longer than {@value OptionData#MAX_CHOICE_NAME_LENGTH}</li>
+     *             <li>The string value of any of the choices is empty or longer than {@value OptionData#MAX_CHOICE_VALUE_LENGTH}</li>
+     *         </ul>
+     *
+     * @return The same callback action, for chaining convenience
+     */
     @Nonnull
     @CheckReturnValue
     default AutoCompleteCallbackAction addChoiceStrings(@Nonnull String... choices)
@@ -73,6 +205,26 @@ public interface AutoCompleteCallbackAction extends InteractionCallbackAction<Vo
                 .collect(Collectors.toList()));
     }
 
+    /**
+     * Add up to {@value OptionData#MAX_CHOICES} choices which can be picked from by the user.
+     * <br>The user may continue writing inputs instead of using one of your choices.
+     *
+     * <p>The provided strings will be used as value and name for the {@link net.dv8tion.jda.api.interactions.commands.Command.Choice Choices}.
+     *
+     * @param  choices
+     *         The choice suggestions to present to the user, each limited to {@value OptionData#MAX_CHOICE_NAME_LENGTH} characters
+     *
+     * @throws IllegalArgumentException
+     *         If any of the following is true:
+     *         <ul>
+     *             <li>{@code null} is provided</li>
+     *             <li>More than {@value OptionData#MAX_CHOICES} are added</li>
+     *             <li>Any of the choice names is empty or longer than {@value OptionData#MAX_CHOICE_NAME_LENGTH}</li>
+     *             <li>The string value of any of the choices is empty or longer than {@value OptionData#MAX_CHOICE_VALUE_LENGTH}</li>
+     *         </ul>
+     *
+     * @return The same callback action, for chaining convenience
+     */
     @Nonnull
     @CheckReturnValue
     default AutoCompleteCallbackAction addChoiceStrings(@Nonnull Collection<String> choices)
@@ -82,6 +234,26 @@ public interface AutoCompleteCallbackAction extends InteractionCallbackAction<Vo
                 .collect(Collectors.toList()));
     }
 
+    /**
+     * Add up to {@value OptionData#MAX_CHOICES} choices which can be picked from by the user.
+     * <br>The user may continue writing inputs instead of using one of your choices.
+     *
+     * <p>The string values of the provided longs will be used as value and name for the {@link net.dv8tion.jda.api.interactions.commands.Command.Choice Choices}.
+     *
+     * @param  choices
+     *         The choice suggestions to present to the user
+     *
+     * @throws IllegalArgumentException
+     *         If any of the following is true:
+     *         <ul>
+     *             <li>{@code null} is provided</li>
+     *             <li>More than {@value OptionData#MAX_CHOICES} are added</li>
+     *             <li>The option type is incompatible with the choice type</li>
+     *             <li>The numeric value of any of the choices is not between {@value  OptionData#MIN_NEGATIVE_NUMBER} and {@value OptionData#MAX_POSITIVE_NUMBER}</li>
+     *         </ul>
+     *
+     * @return The same callback action, for chaining convenience
+     */
     @Nonnull
     @CheckReturnValue
     default AutoCompleteCallbackAction addChoiceLongs(@Nonnull long... choices)
@@ -91,6 +263,26 @@ public interface AutoCompleteCallbackAction extends InteractionCallbackAction<Vo
                 .collect(Collectors.toList()));
     }
 
+    /**
+     * Add up to {@value OptionData#MAX_CHOICES} choices which can be picked from by the user.
+     * <br>The user may continue writing inputs instead of using one of your choices.
+     *
+     * <p>The string values of the provided longs will be used as value and name for the {@link net.dv8tion.jda.api.interactions.commands.Command.Choice Choices}.
+     *
+     * @param  choices
+     *         The choice suggestions to present to the user
+     *
+     * @throws IllegalArgumentException
+     *         If any of the following is true:
+     *         <ul>
+     *             <li>{@code null} is provided</li>
+     *             <li>More than {@value OptionData#MAX_CHOICES} are added</li>
+     *             <li>The option type is incompatible with the choice type</li>
+     *             <li>The numeric value of any of the choices is not between {@value  OptionData#MIN_NEGATIVE_NUMBER} and {@value OptionData#MAX_POSITIVE_NUMBER}</li>
+     *         </ul>
+     *
+     * @return The same callback action, for chaining convenience
+     */
     @Nonnull
     @CheckReturnValue
     default AutoCompleteCallbackAction addChoiceLongs(@Nonnull Collection<Long> choices)
@@ -100,6 +292,26 @@ public interface AutoCompleteCallbackAction extends InteractionCallbackAction<Vo
                 .collect(Collectors.toList()));
     }
 
+    /**
+     * Add up to {@value OptionData#MAX_CHOICES} choices which can be picked from by the user.
+     * <br>The user may continue writing inputs instead of using one of your choices.
+     *
+     * <p>The string values of the provided doubles will be used as value and name for the {@link net.dv8tion.jda.api.interactions.commands.Command.Choice Choices}.
+     *
+     * @param  choices
+     *         The choice suggestions to present to the user
+     *
+     * @throws IllegalArgumentException
+     *         If any of the following is true:
+     *         <ul>
+     *             <li>{@code null} is provided</li>
+     *             <li>More than {@value OptionData#MAX_CHOICES} are added</li>
+     *             <li>The option type is incompatible with the choice type</li>
+     *             <li>The numeric value of any of the choices is not between {@value  OptionData#MIN_NEGATIVE_NUMBER} and {@value OptionData#MAX_POSITIVE_NUMBER}</li>
+     *         </ul>
+     *
+     * @return The same callback action, for chaining convenience
+     */
     @Nonnull
     @CheckReturnValue
     default AutoCompleteCallbackAction addChoiceDoubles(@Nonnull double... choices)
@@ -109,6 +321,26 @@ public interface AutoCompleteCallbackAction extends InteractionCallbackAction<Vo
                 .collect(Collectors.toList()));
     }
 
+    /**
+     * Add up to {@value OptionData#MAX_CHOICES} choices which can be picked from by the user.
+     * <br>The user may continue writing inputs instead of using one of your choices.
+     *
+     * <p>The string values of the provided doubles will be used as value and name for the {@link net.dv8tion.jda.api.interactions.commands.Command.Choice Choices}.
+     *
+     * @param  choices
+     *         The choice suggestions to present to the user
+     *
+     * @throws IllegalArgumentException
+     *         If any of the following is true:
+     *         <ul>
+     *             <li>{@code null} is provided</li>
+     *             <li>More than {@value OptionData#MAX_CHOICES} are added</li>
+     *             <li>The option type is incompatible with the choice type</li>
+     *             <li>The numeric value of any of the choices is not between {@value  OptionData#MIN_NEGATIVE_NUMBER} and {@value OptionData#MAX_POSITIVE_NUMBER}</li>
+     *         </ul>
+     *
+     * @return The same callback action, for chaining convenience
+     */
     @Nonnull
     @CheckReturnValue
     default AutoCompleteCallbackAction addChoiceDoubles(@Nonnull Collection<Double> choices)
