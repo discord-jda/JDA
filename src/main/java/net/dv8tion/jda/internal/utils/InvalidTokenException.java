d9118077dff561997feaa63cc8bbc3c14ce82328
java-coding-prodigy <java-coding-prodigy@github.com>
2022-02-10 12:35:31 +0530

    Some documentation

--- src/main/java/net/dv8tion/jda/internal/utils/InvalidTokenException.java
+++ src/main/java/net/dv8tion/jda/internal/utils/InvalidTokenException.java
@@ -1,14 +1,28 @@
 package net.dv8tion.jda.internal.utils;
 
+/**
+ * This class represents an exception which is thrown when the token provided by the user is invalid
+ * Replaces {@link javax.security.auth.login.LoginException}
+ *
+ * @author java-coding-prodigy
+ * @since 5.0.0
+ * */
 public class InvalidTokenException extends RuntimeException
 {
 
-
+    /**
+     * Constructs an <code>InvalidTokenException</code> with no detail message.
+     * */
     public InvalidTokenException()
     {
         super();
     }
-
+    /**
+     * Constructs a <code>InvalidTokenException</code> with the
+     * specified detail message.
+     *
+     * @param   message   the detail message.
+     */
     public InvalidTokenException(String message)
     {
         super(message);
