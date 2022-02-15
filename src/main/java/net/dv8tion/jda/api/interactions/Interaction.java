8fbee19f8b38717d34e1f801d7f41410ac7a31d7
Xirado <bquickzz0@gmail.com>
2022-02-15 18:36:16 +0100

    Add IModalCallback to Interaction docs

--- src/main/java/net/dv8tion/jda/api/interactions/Interaction.java
+++ src/main/java/net/dv8tion/jda/api/interactions/Interaction.java
@@ -23,6 +23,7 @@ import net.dv8tion.jda.api.interactions.callbacks.IDeferrableCallback;
 import net.dv8tion.jda.api.interactions.callbacks.IMessageEditCallback;
 import net.dv8tion.jda.api.interactions.callbacks.IReplyCallback;
 import net.dv8tion.jda.api.interactions.commands.Command;
+import net.dv8tion.jda.api.interactions.components.text.Modal;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -30,8 +31,8 @@ import java.util.Locale;
 
 /**
  * Abstract representation for any kind of Discord interaction.
- * <br>This includes things such as {@link net.dv8tion.jda.api.interactions.commands.SlashCommandInteraction Slash-Commands}
- * or {@link net.dv8tion.jda.api.interactions.components.buttons.ButtonInteraction Buttons}.
+ * <br>This includes things such as {@link net.dv8tion.jda.api.interactions.commands.SlashCommandInteraction Slash-Commands},
+ * {@link net.dv8tion.jda.api.interactions.components.buttons.ButtonInteraction Buttons} or {@link ModalInteraction Modals}.
  *
  * <p>To properly handle an interaction you must acknowledge it.
  * Each interaction has different callbacks which acknowledge the interaction. These are added by the individual {@code I...Callback} interfaces:
@@ -42,6 +43,8 @@ import java.util.Locale;
  *     <br>Which supports direct message edits and deferred message edits (or no-operation) via {@link IMessageEditCallback#editMessage(String)} and {@link IMessageEditCallback#deferEdit()}</li>
  *     <li>{@link IAutoCompleteCallback}
  *     <br>Which supports choice suggestions for auto-complete interactions via {@link IAutoCompleteCallback#replyChoices(Command.Choice...)}</li>
+ *     <li>{@link net.dv8tion.jda.api.interactions.callbacks.IModalCallback}
+ *     <br>Which supports replying using a {@link net.dv8tion.jda.api.interactions.components.text.Modal Modal} via {@link net.dv8tion.jda.api.interactions.callbacks.IModalCallback#replyModal(Modal)}</li>
  * </ul>
  *
  * <p>Once the interaction is acknowledged, you can not reply with these methods again. If the interaction is a {@link IDeferrableCallback deferrable},
