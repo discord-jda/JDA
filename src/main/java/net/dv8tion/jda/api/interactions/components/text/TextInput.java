13c17fc787c75164105f63e1f729c965bdd62ea7
Xirado <bquickzz0@gmail.com>
2022-02-21 20:48:52 +0100

    Add missing throws in javadocs

--- src/main/java/net/dv8tion/jda/api/interactions/components/text/TextInput.java
+++ src/main/java/net/dv8tion/jda/api/interactions/components/text/TextInput.java
@@ -434,6 +434,9 @@ public interface TextInput extends ActionComponent
         /**
          * Builds a new TextInput from this Builder
          *
+         * @throws IllegalArgumentException
+         *         If maxLength is smaller than minLength
+         *
          * @return the TextInput instance
          */
         @Nonnull
