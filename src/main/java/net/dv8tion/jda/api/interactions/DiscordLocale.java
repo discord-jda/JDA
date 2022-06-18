af50f6a46b621184d495416b2637cfbded2da2a0
freya02 <41875020+freya022@users.noreply.github.com>
2022-06-18 20:34:09 +0200

    Add langs

--- src/main/java/net/dv8tion/jda/api/interactions/DiscordLocale.java
+++ src/main/java/net/dv8tion/jda/api/interactions/DiscordLocale.java
@@ -22,10 +22,36 @@ import java.util.Locale;
 //TODO docs
 public enum DiscordLocale
 {
+    DANISH("da","Danish","Dansk"),
+    GERMAN("de","German","Deutsch"),
     UK("en-GB","English, UK","English, UK"),
     US("en-US","English, US","English, US"),
-    FRENCH("fr","French","Français");
-    //TODO add langs
+    SPANISH("es-ES","Spanish","Español"),
+    FRENCH("fr","French","Français"),
+    CROATIAN("hr","Croatian","Hrvatski"),
+    ITALIAN("it","Italian","Italiano"),
+    LITHUANIAN("lt","Lithuanian","Lietuviškai"),
+    HUNGARIAN("hu","Hungarian","Magyar"),
+    DUTCH("nl","Dutch","Nederlands"),
+    NORWEGIAN("no","Norwegian","Norsk"),
+    POLISH("pl","Polish","Polski"),
+    BRAZILIAN("pt-BR","Portuguese, Brazilian","Português do Brasil"),
+    ROMANIA("ro","Romanian, Romania","Română"),
+    FINNISH("fi","Finnish","Suomi"),
+    SWEDISH("sv-SE","Swedish","Svenska"),
+    VIETNAMESE("vi","Vietnamese","Tiếng Việt"),
+    TURKISH("tr","Turkish","Türkçe"),
+    CZECH("cs","Czech","Čeština"),
+    GREEK("el","Greek","Ελληνικά"),
+    BULGARIAN("bg","Bulgarian","български"),
+    RUSSIAN("ru","Russian","Pусский"),
+    UKRAINIAN("uk","Ukrainian","Українська"),
+    HINDI("hi","Hindi","हिन्दी"),
+    THAI("th","Thai","ไทย"),
+    CHINA("zh-CN","Chinese, China","中文"),
+    JAPANESE("ja","Japanese","日本語"),
+    TAIWAN("zh-TW","Chinese, Taiwan","繁體中文"),
+    KOREAN("ko","Korean","한국어");
 
     private final String locale;
     private final String languageName;
