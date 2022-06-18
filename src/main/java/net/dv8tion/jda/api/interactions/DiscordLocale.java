96fd4cd6dc7e21fd4501521805fd2008663b0b15
freya02 <41875020+freya022@users.noreply.github.com>
2022-06-18 21:29:01 +0200

    Reformat

--- src/main/java/net/dv8tion/jda/api/interactions/DiscordLocale.java
+++ src/main/java/net/dv8tion/jda/api/interactions/DiscordLocale.java
@@ -24,36 +24,36 @@ import java.util.Locale;
  */
 public enum DiscordLocale
 {
-    DANISH("da","Danish","Dansk"),
-    GERMAN("de","German","Deutsch"),
-    UK("en-GB","English, UK","English, UK"),
-    US("en-US","English, US","English, US"),
-    SPANISH("es-ES","Spanish","Español"),
-    FRENCH("fr","French","Français"),
-    CROATIAN("hr","Croatian","Hrvatski"),
-    ITALIAN("it","Italian","Italiano"),
-    LITHUANIAN("lt","Lithuanian","Lietuviškai"),
-    HUNGARIAN("hu","Hungarian","Magyar"),
-    DUTCH("nl","Dutch","Nederlands"),
-    NORWEGIAN("no","Norwegian","Norsk"),
-    POLISH("pl","Polish","Polski"),
-    BRAZILIAN("pt-BR","Portuguese, Brazilian","Português do Brasil"),
-    ROMANIA("ro","Romanian, Romania","Română"),
-    FINNISH("fi","Finnish","Suomi"),
-    SWEDISH("sv-SE","Swedish","Svenska"),
-    VIETNAMESE("vi","Vietnamese","Tiếng Việt"),
-    TURKISH("tr","Turkish","Türkçe"),
-    CZECH("cs","Czech","Čeština"),
-    GREEK("el","Greek","Ελληνικά"),
-    BULGARIAN("bg","Bulgarian","български"),
-    RUSSIAN("ru","Russian","Pусский"),
-    UKRAINIAN("uk","Ukrainian","Українська"),
-    HINDI("hi","Hindi","हिन्दी"),
-    THAI("th","Thai","ไทย"),
-    CHINA("zh-CN","Chinese, China","中文"),
-    JAPANESE("ja","Japanese","日本語"),
-    TAIWAN("zh-TW","Chinese, Taiwan","繁體中文"),
-    KOREAN("ko","Korean","한국어"),
+    DANISH("da", "Danish", "Dansk"),
+    GERMAN("de", "German", "Deutsch"),
+    UK("en-GB", "English, UK", "English, UK"),
+    US("en-US", "English, US", "English, US"),
+    SPANISH("es-ES", "Spanish", "Español"),
+    FRENCH("fr", "French", "Français"),
+    CROATIAN("hr", "Croatian", "Hrvatski"),
+    ITALIAN("it", "Italian", "Italiano"),
+    LITHUANIAN("lt", "Lithuanian", "Lietuviškai"),
+    HUNGARIAN("hu", "Hungarian", "Magyar"),
+    DUTCH("nl", "Dutch", "Nederlands"),
+    NORWEGIAN("no", "Norwegian", "Norsk"),
+    POLISH("pl", "Polish", "Polski"),
+    BRAZILIAN("pt-BR", "Portuguese, Brazilian", "Português do Brasil"),
+    ROMANIA("ro", "Romanian, Romania", "Română"),
+    FINNISH("fi", "Finnish", "Suomi"),
+    SWEDISH("sv-SE", "Swedish", "Svenska"),
+    VIETNAMESE("vi", "Vietnamese", "Tiếng Việt"),
+    TURKISH("tr", "Turkish", "Türkçe"),
+    CZECH("cs", "Czech", "Čeština"),
+    GREEK("el", "Greek", "Ελληνικά"),
+    BULGARIAN("bg", "Bulgarian", "български"),
+    RUSSIAN("ru", "Russian", "Pусский"),
+    UKRAINIAN("uk", "Ukrainian", "Українська"),
+    HINDI("hi", "Hindi", "हिन्दी"),
+    THAI("th", "Thai", "ไทย"),
+    CHINA("zh-CN", "Chinese, China", "中文"),
+    JAPANESE("ja", "Japanese", "日本語"),
+    TAIWAN("zh-TW", "Chinese, Taiwan", "繁體中文"),
+    KOREAN("ko", "Korean", "한국어"),
     UNKNOWN("unknown", "Unknown", "Unknown");
 
     private final String locale;
@@ -106,10 +106,12 @@ public enum DiscordLocale
      * @return The DiscordLocale constant or {@link #UNKNOWN}
      */
     @Nonnull
-    public static DiscordLocale from(String localeTag) {
+    public static DiscordLocale from(String localeTag)
+    {
         for (DiscordLocale discordLocale : values())
         {
-            if (discordLocale.locale.equals(localeTag)) {
+            if (discordLocale.locale.equals(localeTag))
+            {
                 return discordLocale;
             }
         }
@@ -126,7 +128,8 @@ public enum DiscordLocale
      * @return The DiscordLocale constant or {@link #UNKNOWN}
      */
     @Nonnull
-    public static DiscordLocale from(Locale locale) {
+    public static DiscordLocale from(Locale locale)
+    {
         return from(locale.toLanguageTag());
     }
 }
