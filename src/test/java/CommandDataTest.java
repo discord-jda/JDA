6b956bc4b68f9742a93833421d796f49a07a2510
Xirado <bquickzz0@gmail.com>
2022-06-26 09:33:53 +0200

    Move test into a separate Test method

--- src/test/java/CommandDataTest.java
+++ src/test/java/CommandDataTest.java
@@ -49,14 +49,6 @@ public class CommandDataTest
         Assertions.assertFalse(data.getBoolean("dm_permission"));
         Assertions.assertEquals(Permission.BAN_MEMBERS.getRawValue(), data.getUnsignedLong("default_member_permissions"));
 
-        command.setDefaultPermissions(DefaultMemberPermissions.DISABLED);
-        data = command.toData();
-        Assertions.assertEquals(0, data.getUnsignedLong("default_member_permissions"));
-
-        command.setDefaultPermissions(DefaultMemberPermissions.ENABLED);
-        data = command.toData();
-        Assertions.assertTrue(data.isNull("default_member_permissions"));
-
         DataArray options = data.getArray("options");
 
         DataObject option = options.getObject(0);
@@ -73,6 +65,20 @@ public class CommandDataTest
         Assertions.assertFalse(option.getBoolean("required"));
         Assertions.assertEquals("days", option.getString("name"));
         Assertions.assertEquals("The duration of the ban", option.getString("description"));
+    }
+
+    @Test
+    public void testDefaultMemberPermissions()
+    {
+        CommandData command = new CommandDataImpl("ban", "Ban a user from this server")
+                .setDefaultPermissions(DefaultMemberPermissions.DISABLED);
+        DataObject data = command.toData();
+
+        Assertions.assertEquals(0, data.getUnsignedLong("default_member_permissions"));
+
+        command.setDefaultPermissions(DefaultMemberPermissions.ENABLED);
+        data = command.toData();
+        Assertions.assertTrue(data.isNull("default_member_permissions"));
     }
 
     @Test
