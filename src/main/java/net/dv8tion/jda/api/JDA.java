5b24fcc99f17bf68e9a86b4e2cc054dae8215a47
Austin Keener <keeneraustin@yahoo.com>
2022-07-02 20:43:16 -0400

    Fixed 6+ year old typo

--- src/main/java/net/dv8tion/jda/api/JDA.java
+++ src/main/java/net/dv8tion/jda/api/JDA.java
@@ -62,7 +62,7 @@ import java.util.function.Consumer;
 import java.util.regex.Matcher;
 
 /**
- * The core of JDA. Acts as a registry system of JDA. All parts of the the API can be accessed starting from this class.
+ * The core of JDA. Acts as a registry system of JDA. All parts of the API can be accessed starting from this class.
  *
  * @see JDABuilder
  */
