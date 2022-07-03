888f3cbc7f1e36d1c03c9b8682739a191cd117e6
Daedalus <16168171+RedDaedalus@users.noreply.github.com>
2022-07-03 13:42:28 -0600

    revert last commit back to Minn's changes

--- src/main/java/net/dv8tion/jda/api/entities/MessageType.java
+++ src/main/java/net/dv8tion/jda/api/entities/MessageType.java
@@ -26,35 +26,35 @@ public enum MessageType
     /**
      * The normal text messages received when a user or bot sends a Message.
      */
-    DEFAULT(0, false),
+    DEFAULT(0, false, true),
 
     /**
      * Specialized messages used for Groups as a System-Message showing that a new User has been added to the Group.
      * Also used in message threads to indicate a member has joined that thread.
      */
-    RECIPIENT_ADD(1),
+    RECIPIENT_ADD(1, true, false),
 
     /**
      * Specialized messages used for Groups as a System-Message showing that a new User has been removed from the Group.
      * Also used in message threads to indicate a member has left that thread.
      */
-    RECIPIENT_REMOVE(2),
+    RECIPIENT_REMOVE(2, true, false),
 
     /**
      * Specialized message used for Groups as a System-Message showing that a Call was started.
      */
-    CALL(3),
+    CALL(3, true, false),
 
     /**
      * Specialized message used for Groups as a System-Message showing that the name of the Group was changed.
      * Also used in message threads to indicate the name of that thread has changed.
      */
-    CHANNEL_NAME_CHANGE(4),
+    CHANNEL_NAME_CHANGE(4, true, false),
 
     /**
      * Specialized message used for Groups as a System-Message showing that the icon of the Group was changed.
      */
-    CHANNEL_ICON_CHANGE(5),
+    CHANNEL_ICON_CHANGE(5, true, false),
 
     /**
      * Specialized message used in MessageChannels as a System-Message to announce new pins
@@ -94,22 +94,22 @@ public enum MessageType
     /**
      * System message related to discovery qualifications.
      */
-    GUILD_DISCOVERY_DISQUALIFIED(14, true),
+    GUILD_DISCOVERY_DISQUALIFIED(14, true, false),
 
     /**
      * System message related to discovery qualifications.
      */
-    GUILD_DISCOVERY_REQUALIFIED(15),
+    GUILD_DISCOVERY_REQUALIFIED(15, true, false),
 
     /**
      * System message related to discovery qualifications.
      */
-    GUILD_DISCOVERY_GRACE_PERIOD_INITIAL_WARNING(16),
+    GUILD_DISCOVERY_GRACE_PERIOD_INITIAL_WARNING(16, true, false),
 
     /**
      * System message related to discovery qualifications.
      */
-    GUILD_DISCOVERY_GRACE_PERIOD_FINAL_WARNING(17),
+    GUILD_DISCOVERY_GRACE_PERIOD_FINAL_WARNING(17, true, false),
 
     /**
      * This is sent to a TextChannel when a message thread is created if the message from which the thread was started is "old".
@@ -120,12 +120,12 @@ public enum MessageType
     /**
      * Reply to another message. This usually comes with a {@link Message#getReferencedMessage() referenced message}.
      */
-    INLINE_REPLY(19, false),
+    INLINE_REPLY(19, false, true),
 
     /**
      * This message was created by an interaction. Usually in combination with Slash Commands.
      */
-    SLASH_COMMAND(20, false),
+    SLASH_COMMAND(20, false, true),
 
     /**
      * A new message sent as the first message in threads that are started from an existing message in the parent channel.
@@ -141,7 +141,7 @@ public enum MessageType
     /**
      * This message was created by an interaction. Usually in combination with Context Menus.
      */
-    CONTEXT_COMMAND(23, false),
+    CONTEXT_COMMAND(23, false, true),
 
     /**
      * This message was created by the automod system.
@@ -164,16 +164,6 @@ public enum MessageType
         this.id = id;
         this.system = system;
         this.deletable = deletable;
-    }
-
-    MessageType(int id, boolean system) {
-        // most system messages are not deletable, most non-system messages are
-        this(id, system, !system);
-    }
-
-    MessageType(int id) {
-        // most messages are system messages
-        this(id, true);
     }
 
     /**
