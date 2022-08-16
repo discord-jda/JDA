abbcda0abcfee926e0e7187ef484703a9f038678
Mitmocc <33007498+Mitmocc@users.noreply.github.com>
2022-08-16 20:07:33 +0200

    reverting some latest master changes so I don't have to rewrite while testing

--- src/main/java/net/dv8tion/jda/internal/requests/restaction/GuildScheduledEventActionImpl.java
+++ src/main/java/net/dv8tion/jda/internal/requests/restaction/GuildScheduledEventActionImpl.java
@@ -150,13 +150,13 @@ public class GuildScheduledEventActionImpl extends AuditableRestActionImpl<Guild
     {
         Checks.check(name != null, "Missing required parameter: Name");
         Checks.check(startTime != null, "Missing required parameter: Start Time");
-        Checks.check(endTime == null && entityType != 3, "Invalid parameter: End Time (Only valid for external location)");
         Checks.check(entityType != 3 || (location != null && location.length() > 0 && endTime != null), "Missing required parameter: End Time");
         Checks.check(entityType == 1 || entityType == 2 || (entityType == 3 && location != null && location.length() > 0), "Missing required parameter: Location");
         Checks.check(startTime.isAfter(OffsetDateTime.now()), "Cannot schedule event in the past!");
 
         if (endTime != null)
             Checks.check((endTime).isAfter(startTime), "Cannot schedule event to end before starting!");
+            Checks.check(entityType == 3, "Invalid parameter: End Time (Only valid for external location)");
     }
 
 
