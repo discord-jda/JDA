9b069ea2d3dc72d65f41e90b9f18f854c5c64bf5
freya02 <41875020+freya022@users.noreply.github.com>
2022-10-16 14:33:11 +0200

    Update src/main/java/net/dv8tion/jda/api/entities/Message.java
    Co-authored-by: Austin Keener <keeneraustin@yahoo.com>

--- src/main/java/net/dv8tion/jda/api/entities/Message.java
+++ src/main/java/net/dv8tion/jda/api/entities/Message.java
@@ -1993,8 +1993,8 @@ public interface Message extends ISnowflake, Formattable
         EVERYONE("@everyone", "everyone"),
         /**
          * Represents a mention for a slash command.
-         * <br>The first group is the command name, the second group is the subcommand group (nullable),
-         * the third group is the subcommand (nullable), and the fourth group is the command ID.
+         * <br>The first group is the command name, the second group is the subcommand group name (nullable),
+         * the third group is the subcommand name (nullable), and the fourth group is the command ID.
          */
         SLASH_COMMAND("</([\\w-]+)(?> ([\\w-]+))??(?> ([\\w-]+))?:(\\d+)>", null);
 
