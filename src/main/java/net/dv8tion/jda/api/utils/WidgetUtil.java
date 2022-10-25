dbfcf97774be8f18be92079bf53b5dae6a74911b
Florian Spieß <business@minnced.club>
2022-10-25 15:29:03 +0200

    Fix compile errors

--- src/main/java/net/dv8tion/jda/api/utils/WidgetUtil.java
+++ src/main/java/net/dv8tion/jda/api/utils/WidgetUtil.java
@@ -20,10 +20,10 @@ import gnu.trove.map.hash.TLongObjectHashMap;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.exceptions.RateLimitedException;
+import net.dv8tion.jda.api.requests.RestConfig;
 import net.dv8tion.jda.api.utils.data.DataArray;
 import net.dv8tion.jda.api.utils.data.DataObject;
 import net.dv8tion.jda.internal.entities.EntityBuilder;
-import net.dv8tion.jda.internal.requests.Requester;
 import net.dv8tion.jda.internal.utils.Checks;
 import net.dv8tion.jda.internal.utils.EntityString;
 import net.dv8tion.jda.internal.utils.Helpers;
@@ -51,8 +51,8 @@ import java.util.List;
  */
 public class WidgetUtil 
 {
-    public static final String WIDGET_PNG = Requester.DISCORD_API_PREFIX + "guilds/%s/widget.png?style=%s";
-    public static final String WIDGET_URL = Requester.DISCORD_API_PREFIX + "guilds/%s/widget.json";
+    public static final String WIDGET_PNG = RestConfig.DEFAULT_BASE_URL + "guilds/%s/widget.png?style=%s";
+    public static final String WIDGET_URL = RestConfig.DEFAULT_BASE_URL + "guilds/%s/widget.json";
     public static final String WIDGET_HTML = "<iframe src=\"https://discord.com/widget?id=%s&theme=%s\" width=\"%d\" height=\"%d\" allowtransparency=\"true\" frameborder=\"0\"></iframe>";
     
     /**
@@ -211,7 +211,7 @@ public class WidgetUtil
         Request request = new Request.Builder()
                     .url(String.format(WIDGET_URL, guildId))
                     .method("GET", null)
-                    .header("user-agent", Requester.USER_AGENT)
+                    .header("user-agent", RestConfig.USER_AGENT)
                     .header("accept-encoding", "gzip")
                     .build();
 
