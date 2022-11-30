e3243ff96189bad5c7072c412a551c0399d1dd2a
Florian Spieß <business@minnced.club>
2022-11-30 22:07:40 +0100

    Remove code to rename guild_member to member

--- src/main/java/net/dv8tion/jda/internal/entities/channel/concrete/ThreadChannelImpl.java
+++ src/main/java/net/dv8tion/jda/internal/entities/channel/concrete/ThreadChannelImpl.java
@@ -204,7 +204,7 @@ public class ThreadChannelImpl extends AbstractGuildChannelImpl<ThreadChannelImp
                 () -> {
                     Route.CompiledRoute route = Route.Channels.GET_THREAD_MEMBER.compile(getId(), Long.toUnsignedString(id)).withQueryParams("with_member", "true");
                     return new RestActionImpl<>(jda, route, (resp, req) ->
-                        jda.getEntityBuilder().createThreadMember(getGuild(), this, resp.getObject().rename("guild_member", "member")));
+                        jda.getEntityBuilder().createThreadMember(getGuild(), this, resp.getObject()));
                 });
     }
 
