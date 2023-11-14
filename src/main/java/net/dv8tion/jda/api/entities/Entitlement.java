020f464d6e24291b040cd6c3e5a62f8e248c7123
Giuliopime <60524738+Giuliopime@users.noreply.github.com>
2023-11-14 11:53:35 +0100

    fix: copyright

--- src/main/java/net/dv8tion/jda/api/entities/Entitlement.java
+++ src/main/java/net/dv8tion/jda/api/entities/Entitlement.java
@@ -1,14 +1,27 @@
-package net.dv8tion.jda.api.entities;
+/*
+ * Copyright 2015 Austin Keener, Michael Ritter, Florian Spieß, and the JDA contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+package net.dv8tion.jda.api.entities;
 
 import javax.annotation.Nullable;
 import java.time.OffsetDateTime;
 
 /**
- * Represents a Discord {@link Entitlement Entitlement} for premium App subscriptions.
+ * Represents a Discord Entitlement for premium App subscriptions.
  * <br>This should contain all information provided from Discord about an {@link Entitlement Entitlement}.
- *
- * @author Giulio Pimenoff V.
  */
 public interface Entitlement
 {
