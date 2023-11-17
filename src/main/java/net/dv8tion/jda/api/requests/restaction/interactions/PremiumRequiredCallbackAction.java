3094a35eac1cd26eeacc1ed607fc59e85c0a68b6
Giuliopime <60524738+Giuliopime@users.noreply.github.com>
2023-11-17 23:36:18 +0100

    Update src/main/java/net/dv8tion/jda/api/requests/restaction/interactions/PremiumRequiredCallbackAction.java
    Co-authored-by: freya02 <41875020+freya022@users.noreply.github.com>

--- src/main/java/net/dv8tion/jda/api/requests/restaction/interactions/PremiumRequiredCallbackAction.java
+++ src/main/java/net/dv8tion/jda/api/requests/restaction/interactions/PremiumRequiredCallbackAction.java
@@ -21,7 +21,7 @@ import net.dv8tion.jda.api.requests.FluentRestAction;
 /**
  * An {@link InteractionCallbackAction} that can be used to send the "Premium required" interaction response.
  *
- * @see    net.dv8tion.jda.api.interactions.callbacks.IPremiumRequiredReplyCallback
+ * @see net.dv8tion.jda.api.interactions.callbacks.IPremiumRequiredReplyCallback
  */
 public interface PremiumRequiredCallbackAction extends InteractionCallbackAction<Void>, FluentRestAction<Void, PremiumRequiredCallbackAction>
 {
