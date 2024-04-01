fb290ea79bb505ff3b90ca867ed947c3df75ad86
Florian Spieß <business@minn.dev>
2024-04-01 12:30:15 +0200

    Add getTestEmbed

--- src/test/java/net/dv8tion/jda/entities/MessageSerializationTest.java
+++ src/test/java/net/dv8tion/jda/entities/MessageSerializationTest.java
@@ -21,27 +21,35 @@ import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.utils.data.DataArray;
 import net.dv8tion.jda.api.utils.data.DataObject;
 import net.dv8tion.jda.util.PrettyRepresentation;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MessageSerializationTest
 {
+
+    private static final String DESCRIPTION_TEXT = "Description Text";
+    private static final String TITLE_TEXT = "Title Text";
+    private static final String TITLE_URL = "https://example.com/title";
+    private static final String AUTHOR_TEXT = "Author Text";
+    private static final String AUTHOR_URL = "https://example.com/author";
+    private static final String AUTHOR_ICON = "https://example.com/author_icon";
+    private static final String FOOTER_TEXT = "Footer Text";
+    private static final String FOOTER_ICON = "https://example.com/footer_icon";
+    private static final String IMAGE_URL = "https://example.com/image";
+    private static final String THUMBNAIL_URL = "https://example.com/thumbnail";
+    private static final String FIELD_1_NAME = "Field 1";
+    private static final String FIELD_1_TEXT = "Field 1 Text";
+    private static final String FIELD_2_NAME = "Field 2";
+    private static final String FIELD_2_TEXT = "Field 2 Text";
+    private static final String FIELD_3_NAME = "Field 3";
+    private static final String FIELD_3_TEXT = "Field 3 Text";
+
     @Test
     void testEmbedSerialization()
     {
-        EmbedBuilder builder = new EmbedBuilder();
-        builder.setDescription("Description Text");
-        builder.setTitle("Title Text", "https://example.com/title");
-        builder.setAuthor("Author Text", "https://example.com/author", "https://example.com/author_icon");
-        builder.setFooter("Footer Text", "https://example.com/footer_icon");
-        builder.setImage("https://example.com/image");
-        builder.setThumbnail("https://example.com/thumbnail");
-        builder.addField("Field 1", "Field 1 Text", true);
-        builder.addField("Field 2", "Field 2 Text", false);
-        builder.addField("Field 3", "Field 3 Text", true);
-
-        MessageEmbed embed = builder.build();
+        MessageEmbed embed = getTestEmbed();
 
         MessageEmbed dataEmbed = EmbedBuilder.fromData(embed.toData()).build();
 
@@ -51,32 +59,48 @@ public class MessageSerializationTest
         assertThat(dataEmbed.toData())
             .withRepresentation(new PrettyRepresentation())
             .isEqualTo(DataObject.empty()
-                .put("title", "Title Text")
-                .put("url", "https://example.com/title")
-                .put("description", "Description Text")
+                .put("title", TITLE_TEXT)
+                .put("url", TITLE_URL)
+                .put("description", DESCRIPTION_TEXT)
                 .put("image", DataObject.empty()
-                    .put("url", "https://example.com/image"))
+                    .put("url", IMAGE_URL))
                 .put("thumbnail", DataObject.empty()
-                    .put("url", "https://example.com/thumbnail"))
+                    .put("url", THUMBNAIL_URL))
                 .put("footer", DataObject.empty()
-                    .put("icon_url", "https://example.com/footer_icon")
-                    .put("text", "Footer Text"))
+                    .put("icon_url", FOOTER_ICON)
+                    .put("text", FOOTER_TEXT))
                 .put("author", DataObject.empty()
-                    .put("icon_url", "https://example.com/author_icon")
-                    .put("name", "Author Text")
-                    .put("url", "https://example.com/author"))
+                    .put("icon_url", AUTHOR_ICON)
+                    .put("name", AUTHOR_TEXT)
+                    .put("url", AUTHOR_URL))
                 .put("fields", DataArray.empty()
                     .add(DataObject.empty()
                         .put("inline", true)
-                        .put("name", "Field 1")
-                        .put("value", "Field 1 Text"))
+                        .put("name", FIELD_1_NAME)
+                        .put("value", FIELD_1_TEXT))
                     .add(DataObject.empty()
                         .put("inline", false)
-                        .put("name", "Field 2")
-                        .put("value", "Field 2 Text"))
+                        .put("name", FIELD_2_NAME)
+                        .put("value", FIELD_2_TEXT))
                     .add(DataObject.empty()
                         .put("inline", true)
-                        .put("name", "Field 3")
-                        .put("value", "Field 3 Text"))));
+                        .put("name", FIELD_3_NAME)
+                        .put("value", FIELD_3_TEXT))));
+    }
+
+    @NotNull
+    private static MessageEmbed getTestEmbed()
+    {
+        return new EmbedBuilder()
+                .setDescription(DESCRIPTION_TEXT)
+                .setTitle(TITLE_TEXT, TITLE_URL)
+                .setAuthor(AUTHOR_TEXT, AUTHOR_URL, AUTHOR_ICON)
+                .setFooter(FOOTER_TEXT, FOOTER_ICON)
+                .setImage(IMAGE_URL)
+                .setThumbnail(THUMBNAIL_URL)
+                .addField(FIELD_1_NAME, FIELD_1_TEXT, true)
+                .addField(FIELD_2_NAME, FIELD_2_TEXT, false)
+                .addField(FIELD_3_NAME, FIELD_3_TEXT, true)
+                .build();
     }
 }
