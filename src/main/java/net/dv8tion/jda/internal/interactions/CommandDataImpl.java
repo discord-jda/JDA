6829669ba34e6b18c264991a30ccd192c935f027
freya02 <41875020+freya022@users.noreply.github.com>
2024-05-05 23:15:18 +0200

    Run localization mapper before getting options data

--- src/main/java/net/dv8tion/jda/internal/interactions/CommandDataImpl.java
+++ src/main/java/net/dv8tion/jda/internal/interactions/CommandDataImpl.java
@@ -94,9 +94,9 @@ public class CommandDataImpl implements SlashCommandData
     @Override
     public DataObject toData()
     {
-        DataArray options = DataArray.fromCollection(this.options);
-
         if (localizationMapper != null) localizationMapper.localizeCommand(this);
+
+        DataArray options = DataArray.fromCollection(this.options);
 
         DataObject json = DataObject.empty()
                 .put("type", type.getId())
