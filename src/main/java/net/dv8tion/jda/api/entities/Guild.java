addf922ddd0f52ff1c00362be2f63638b767b6cc
V Play Games <74888319+V-Play-Games@users.noreply.github.com>
2024-07-19 19:07:19 +0530

    Add Guild#retrieveScheduledEvents

--- src/main/java/net/dv8tion/jda/api/entities/Guild.java
+++ src/main/java/net/dv8tion/jda/api/entities/Guild.java
@@ -1440,6 +1440,15 @@ public interface Guild extends IGuildChannelContainer<GuildChannel>, ISnowflake
      */
     @Nonnull
     SortedSnowflakeCacheView<ScheduledEvent> getScheduledEventCache();
+
+    /**
+     * Retrieves all {@link ScheduledEvent ScheduledEvents} for this guild.
+     *
+     * @return {@link RestAction} - Type: {@link List} of {@link ScheduledEvent}
+     */
+    @Nonnull
+    @CheckReturnValue
+    RestAction<@Unmodifiable List<ScheduledEvent>> retrieveScheduledEvents();
     
     /**
      * Gets a list of all {@link ScheduledEvent ScheduledEvents} in this Guild that have the same
