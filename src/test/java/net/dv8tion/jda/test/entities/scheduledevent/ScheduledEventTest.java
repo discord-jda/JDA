6703919ebfd6c25d0839989c4427ad8b67c04ecb
Florian Spieß <business@minn.dev>
2024-10-05 18:12:58 +0200

    Cleanup ScheduledEvent#getJumpUrl

--- src/test/java/net/dv8tion/jda/test/entities/scheduledevent/ScheduledEventTest.java
+++ src/test/java/net/dv8tion/jda/test/entities/scheduledevent/ScheduledEventTest.java
@@ -1,29 +1,49 @@
+/*
+ * Copyright 2015 Austin Keener, Michael Ritter, Florian Spieß, and the JDA contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.dv8tion.jda.test.entities.scheduledevent;
 
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.ScheduledEvent;
-import net.dv8tion.jda.internal.JDAImpl;
-import net.dv8tion.jda.internal.entities.GuildImpl;
 import net.dv8tion.jda.internal.entities.ScheduledEventImpl;
-import org.junit.jupiter.api.Assertions;
+import net.dv8tion.jda.test.Constants;
+import net.dv8tion.jda.test.IntegrationTest;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 
-public class ScheduledEventTest{
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+public class ScheduledEventTest extends IntegrationTest
+{
+    @Mock
+    Guild guild;
 
     @Test
-    void testGetJumpUrl(){
+    void testGetJumpUrl()
+    {
+        String eventId = randomSnowflake();
+        String guildId = Long.toUnsignedString(Constants.GUILD_ID);
 
-        long guildId = 7777777;
-        long eventId = 333;
+        when(guild.getId()).thenReturn(guildId);
 
-        String expected = "https://discord.com/events/" + guildId + "/" + eventId;
+        ScheduledEvent scheduledEvent = new ScheduledEventImpl(Long.parseUnsignedLong(eventId), guild);
 
-        Guild guild = new GuildImpl(new JDAImpl(null), guildId);
-
-        ScheduledEvent scheduledEvent = new ScheduledEventImpl(eventId, guild);
-
-        Assertions.assertEquals(expected, scheduledEvent.getJumpUrl());
-
+        assertThat(scheduledEvent.getJumpUrl())
+            .isEqualTo("https://discord.com/events/" + guildId + "/" + eventId);
     }
 
 }
