0af3c2f4469ed92ba5c6cb0171ced71f501a46e2
freya02 <41875020+freya022@users.noreply.github.com>
2024-11-15 22:06:42 +0100

    Add CommandLevelUserOrRolePermissionChecks tests

--- src/test/java/net/dv8tion/jda/test/interactions/privileges/AbstractPrivilegeConfigTest.java
+++ src/test/java/net/dv8tion/jda/test/interactions/privileges/AbstractPrivilegeConfigTest.java
@@ -18,6 +18,7 @@ package net.dv8tion.jda.test.interactions.privileges;
 
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.entities.channel.middleman.GuildChannel;
 import net.dv8tion.jda.api.interactions.commands.Command;
 import net.dv8tion.jda.api.interactions.commands.PrivilegeConfig;
@@ -35,9 +36,9 @@ import org.mockito.Mock;
 import org.mockito.MockedStatic;
 
 import java.util.Arrays;
+import java.util.Collections;
 
-import static org.mockito.Mockito.mockStatic;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public abstract class AbstractPrivilegeConfigTest extends IntegrationTest
 {
@@ -65,8 +66,19 @@ public abstract class AbstractPrivilegeConfigTest extends IntegrationTest
     void setupMocks()
     {
         when(jda.getSelfUser()).thenReturn(new SelfUserImpl(Constants.BUTLER_USER_ID, jda));
+
+        Role everyoneRole = mock(Role.class);
+        when(everyoneRole.getIdLong()).thenReturn(Constants.GUILD_ID);
         when(guild.getIdLong()).thenReturn(Constants.GUILD_ID);
+        when(guild.getPublicRole()).thenReturn(everyoneRole);
+
         when(channel.getIdLong()).thenReturn(Constants.CHANNEL_ID);
+        when(channel.getGuild()).thenReturn(guild);
+
+        Role memberRole = mock(Role.class);
+        when(memberRole.getIdLong()).thenReturn(Constants.ROLE_ID);
+        when(member.getIdLong()).thenReturn(Constants.MINN_USER_ID);
+        when(member.getRoles()).thenReturn(Collections.singletonList(memberRole));
 
         commandLevelChannelMock = mockStatic(CommandLevelChannelPermissionChecks.class);
         appLevelChannelMock = mockStatic(AppLevelChannelPermissionChecks.class);
@@ -112,5 +124,20 @@ public abstract class AbstractPrivilegeConfigTest extends IntegrationTest
     protected IntegrationPrivilege allChannelsPrivilege(boolean enabled)
     {
         return new IntegrationPrivilege(guild, IntegrationPrivilege.Type.CHANNEL, enabled, Constants.GUILD_ID - 1);
+    }
+
+    protected IntegrationPrivilege userPrivilege(boolean enabled)
+    {
+        return new IntegrationPrivilege(guild, IntegrationPrivilege.Type.USER, enabled, Constants.MINN_USER_ID);
+    }
+
+    protected IntegrationPrivilege rolePrivilege(boolean enabled)
+    {
+        return new IntegrationPrivilege(guild, IntegrationPrivilege.Type.ROLE, enabled, Constants.ROLE_ID);
+    }
+
+    protected IntegrationPrivilege everyonePrivilege(boolean enabled)
+    {
+        return new IntegrationPrivilege(guild, IntegrationPrivilege.Type.ROLE, enabled, guild.getIdLong());
     }
 }
