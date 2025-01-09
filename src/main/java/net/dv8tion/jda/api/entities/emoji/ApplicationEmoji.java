59c80384e7892dfcae64bddb6c625a3583879ebf
freya02 <41875020+freya022@users.noreply.github.com>
2025-01-09 17:41:46 +0100

    Rename ApplicationEmoji#APPLICATION_EMOJI_CAP -> MAX_APPLICATION_EMOJIS

--- src/main/java/net/dv8tion/jda/api/entities/emoji/ApplicationEmoji.java
+++ src/main/java/net/dv8tion/jda/api/entities/emoji/ApplicationEmoji.java
@@ -16,6 +16,7 @@
 
 package net.dv8tion.jda.api.entities.emoji;
 
+import net.dv8tion.jda.annotations.ReplaceWith;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Icon;
 import net.dv8tion.jda.api.entities.User;
@@ -38,7 +39,17 @@ import javax.annotation.Nullable;
  */
 public interface ApplicationEmoji extends CustomEmoji
 {
+    /**
+     * @deprecated Replaced with {@link #MAX_APPLICATION_EMOJIS}
+     */
+    @Deprecated
+    @ReplaceWith("MAX_APPLICATION_EMOJIS")
     int APPLICATION_EMOJI_CAP = 2000;
+
+    /**
+     * Maximum number of emojis that can be registered on an application.
+     */
+    int MAX_APPLICATION_EMOJIS = 2000;
 
     /**
      * The {@link net.dv8tion.jda.api.JDA JDA} instance of this emoji
