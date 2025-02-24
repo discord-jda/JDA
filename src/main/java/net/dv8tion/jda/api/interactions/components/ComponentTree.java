c0ed580f97dd0c0370e3220ad7942fa19a3a988d
freya02 <41875020+freya022@users.noreply.github.com>
2025-02-24 21:41:16 +0100

    Align ComponentTree(Impl) with how layout components are constructed

--- src/main/java/net/dv8tion/jda/api/interactions/components/ComponentTree.java
+++ src/main/java/net/dv8tion/jda/api/interactions/components/ComponentTree.java
@@ -1,13 +1,26 @@
 package net.dv8tion.jda.api.interactions.components;
 
 import net.dv8tion.jda.api.interactions.components.replacer.ComponentReplacer;
+import net.dv8tion.jda.internal.interactions.components.ComponentTreeImpl;
 
 import javax.annotation.CheckReturnValue;
 import javax.annotation.Nonnull;
+import java.util.Arrays;
+import java.util.Collection;
 
 public interface ComponentTree
 {
     @Nonnull
     @CheckReturnValue
     <T extends Component> ComponentTree replace(ComponentReplacer<T> replacer);
+
+    static ComponentTree of(Collection<? extends Component> components)
+    {
+        return ComponentTreeImpl.of(components);
+    }
+
+    static ComponentTree of(Component... components)
+    {
+        return of(Arrays.asList(components));
+    }
 }
