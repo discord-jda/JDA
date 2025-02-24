d7545e22e9a1e33764f6854ca343fab33f773c93
freya02 <41875020+freya022@users.noreply.github.com>
2025-02-24 21:41:16 +0100

    Align ComponentTree(Impl) with how layout components are constructed

--- src/main/java/net/dv8tion/jda/internal/interactions/components/ComponentTreeImpl.java
+++ src/main/java/net/dv8tion/jda/internal/interactions/components/ComponentTreeImpl.java
@@ -2,23 +2,33 @@ package net.dv8tion.jda.internal.interactions.components;
 
 import net.dv8tion.jda.api.interactions.components.Component;
 import net.dv8tion.jda.api.interactions.components.ComponentTree;
+import net.dv8tion.jda.api.interactions.components.ComponentUnion;
 import net.dv8tion.jda.api.interactions.components.replacer.ComponentReplacer;
 import net.dv8tion.jda.internal.interactions.components.replacer.IReplacerAware;
+import net.dv8tion.jda.internal.utils.UnionUtil;
 
 import javax.annotation.Nonnull;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class ComponentTreeImpl implements ComponentTree
 {
-    private final List<Component> components;
+    private final List<ComponentUnion> components;
 
-    public ComponentTreeImpl(List<Component> components)
+    private ComponentTreeImpl(Collection<ComponentUnion> components)
     {
         this.components = new ArrayList<>(components);
     }
 
-    @SuppressWarnings({"unchecked"})
+    public static ComponentTree of(Collection<? extends Component> _components)
+    {
+        final Collection<ComponentUnion> components = UnionUtil.membersToUnion(_components);
+        // TODO-components-v2 - checks?
+
+        return new ComponentTreeImpl(components);
+    }
+
     @Nonnull
     @Override
     public <T extends Component> ComponentTree replace(ComponentReplacer<T> replacer)
