e90ec99d4a75215ddbbed88be54ed1a466e70168
freya02 <41875020+freya022@users.noreply.github.com>
2025-03-06 19:28:45 +0100

    Add static factory for ComponentTree

---
+++ src/main/java/net/dv8tion/jda/internal/interactions/components/ComponentTreeImpl.java
@@ -0,0 +1,33 @@
+package net.dv8tion.jda.internal.interactions.components;
+
+import net.dv8tion.jda.api.interactions.components.Component;
+import net.dv8tion.jda.api.interactions.components.ComponentUnion;
+import net.dv8tion.jda.api.interactions.components.replacer.ComponentReplacer;
+import net.dv8tion.jda.api.interactions.components.utils.ComponentTree;
+import net.dv8tion.jda.internal.interactions.components.replacer.IReplacerAware;
+
+import javax.annotation.Nonnull;
+import java.util.Collection;
+
+public final class ComponentTreeImpl<E extends ComponentUnion> extends AbstractComponentTree<E>
+{
+    private final Class<? extends Component> componentType;
+
+    public ComponentTreeImpl(Class<? extends Component> componentType, Collection<E> components)
+    {
+        super(components);
+        this.componentType = componentType;
+    }
+
+    @Nonnull
+    @Override
+    public ComponentTree<E> replace(ComponentReplacer replacer)
+    {
+        return IReplacerAware.doReplace(
+                componentType,
+                components,
+                replacer,
+                (newComponents) -> new ComponentTreeImpl<>(componentType, newComponents)
+        );
+    }
+}
