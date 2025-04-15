a5a597fd0fd783c72d51c9f1f97c5ff7cb8ed905
Lilly <46890129+RainbowDashLabs@users.noreply.github.com>
2025-04-15 15:08:46 +0200

    Add docs and request parsing

--- src/main/java/net/dv8tion/jda/api/entities/SKU.java
+++ src/main/java/net/dv8tion/jda/api/entities/SKU.java
@@ -1,12 +1,60 @@
 package net.dv8tion.jda.api.entities;
 
+import java.util.Set;
+
+/**
+ * SKUs (stock-keeping units) in Discord represent premium offerings that can be made available to application users or guilds.
+ */
 public interface SKU extends SkuSnowflake
 {
+    /**
+     * Type of the SKU
+     * @return SKU type
+     */
     SKUType getType();
 
+    /**
+     * Customer-facing name of your premium offering
+     * @return SKU name
+     */
     String getName();
 
+    /**
+     * System-generated URL slug based on the SKU's name
+     * @return SKU slug
+     */
     String getSlug();
 
-    int getFlags();
+    /**
+     * Flags can be used to differentiate user and server subscriptions
+     * @return set of flags.
+     */
+    Set<SKUFlag> getFlags();
+
+    /**
+     * Checks whether the SKU is available to be purchased for a guild.
+     * @return true if it's for guilds
+     */
+    default boolean isGuildSKU()
+    {
+        return getFlags().contains(SKUFlag.GUILD_SUBSCRIPTION);
+    }
+
+    /**
+     * Checks whether the SKU is available to be purchased for a user.
+     * @return true if it's for users
+     */
+    default boolean isUserSKU()
+    {
+        return getFlags().contains(SKUFlag.USER_SUBSCRIPTION);
+    }
+
+    /**
+     * Checks whether this SKU is available.
+     * @return true if available
+     */
+    default boolean isAvailable()
+    {
+        return getFlags().contains(SKUFlag.AVAILABLE);
+    }
 }
--- src/main/java/net/dv8tion/jda/internal/entities/SKUImpl.java
+++ src/main/java/net/dv8tion/jda/internal/entities/SKUImpl.java
@@ -1,26 +1,20 @@
 package net.dv8tion.jda.internal.entities;
 
+import net.dv8tion.jda.api.entities.SKU;
+import net.dv8tion.jda.api.entities.SKUFlag;
 import net.dv8tion.jda.api.entities.SKUType;
 
-public class SKUImpl extends SkuSnowflakeImpl implements net.dv8tion.jda.api.entities.SKU
+import java.util.Set;
+
+public class SKUImpl extends SkuSnowflakeImpl implements SKU
 {
     // As described in https://discord.com/developers/docs/resources/sku#sku-object-sku-types
     private final SKUType type;
     private final String name;
     private final String slug;
-    private final int flags;
+    private final Set<SKUFlag> flags;
 
-    /*
-    More undocumented fields are:
-    dependent_sku_id: Probably for up and downgrading of tiers.
-    manifest_label: Unclear
-    access_type: Unclear. Type integer
-    features: Probably an array of strings containing the feature descriptions
-    release_date: The date the SKU was published. Type unclear. UNIX timestamp?
-    show_age_gate: Probably if this SKU has an age gap. Doesnt seem to be supported to far. Type: Boolean
-     */
-
-    public SKUImpl(long id, SKUType type, String name, String slug, int flags)
+    public SKUImpl(long id, SKUType type, String name, String slug, Set<SKUFlag> flags)
     {
         super(id);
         this.type = type;
@@ -48,7 +42,7 @@ public class SKUImpl extends SkuSnowflakeImpl implements net.dv8tion.jda.api.ent
     }
 
     @Override
-    public int getFlags()
+    public Set<SKUFlag> getFlags()
     {
         return flags;
     }
