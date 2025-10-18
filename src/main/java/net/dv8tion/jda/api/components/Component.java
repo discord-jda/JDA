926e94ab86bf2a081e46432b7873db7930b61a47
freya02 <41875020+freya022@users.noreply.github.com>
2025-10-18 15:47:00 +0200

    Add modal file uploads (#2920)

--- src/main/java/net/dv8tion/jda/api/components/Component.java
+++ src/main/java/net/dv8tion/jda/api/components/Component.java
@@ -134,6 +134,7 @@ public interface Component
         SEPARATOR(14, true, false),
         CONTAINER(17, true, false),
         LABEL(18, false, true),
+        FILE_UPLOAD(19, false, true),
         ;
 
         private final int key;
