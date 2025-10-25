fe599d289bee788d4ad00456d36b8dd34d1c71ba
freya02 <41875020+freya022@users.noreply.github.com>
2025-10-25 13:38:29 +0200

    Port #2943

--- buildSrc/src/main/kotlin/net/dv8tion/jda/tasks/javadocs.kt
+++ buildSrc/src/main/kotlin/net/dv8tion/jda/tasks/javadocs.kt
@@ -27,25 +27,9 @@ fun Project.configureJavadoc(
         val effectiveJdkDocsVersion = max(11, targetVersion.majorVersion.toInt())
         links("https://docs.oracle.com/en/java/javase/${effectiveJdkDocsVersion}/docs/api/", "https://takahikokawasaki.github.io/nv-websocket-client/")
 
-        val javaVersion = JavaVersion.current()
-        if (JavaVersion.VERSION_1_8 < javaVersion) {
-            addBooleanOption("html5", true) // Adds search bar
-            addStringOption("-release", targetVersion.majorVersion)
-        }
-
-        // Fix for https://stackoverflow.com/questions/52326318/maven-javadoc-search-redirects-to-undefined-url
-        if (javaVersion in JavaVersion.VERSION_11..JavaVersion.VERSION_12) {
-            addBooleanOption("-no-module-directories", true)
-        }
-
-        // Java 13 changed accessibility rules.
-        // On versions less than Java 13, we simply ignore the errors.
-        // Both of these remove "no comment" warnings.
-        if (javaVersion >= JavaVersion.VERSION_13) {
-            addBooleanOption("Xdoclint:all,-missing", true)
-        } else {
-            addBooleanOption("Xdoclint:all,-missing,-accessibility", true)
-        }
+        addBooleanOption("html5", true) // Adds search bar
+        addStringOption("-release", targetVersion.majorVersion)
+        addBooleanOption("Xdoclint:all,-missing", true)
 
         overview = overviewFile
     }
