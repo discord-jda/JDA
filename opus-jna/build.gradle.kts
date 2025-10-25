7913fe22ee1464b848e45655c4a8aaec36199b53
freya02 <41875020+freya022@users.noreply.github.com>
2025-10-25 11:30:20 +0200

    Set up publishing

--- opus-jna/build.gradle.kts
+++ opus-jna/build.gradle.kts
@@ -14,8 +14,14 @@
  * limitations under the License.
  */
 
+import org.jreleaser.gradle.plugin.tasks.AbstractJReleaserTask
+import org.jreleaser.model.Active
+
 plugins {
     `java-library`
+    `maven-publish`
+
+    alias(libs.plugins.jreleaser)
 }
 
 ////////////////////////////////////
@@ -24,13 +30,19 @@ plugins {
 //                                //
 ////////////////////////////////////
 
+group = rootProject.group
+version = rootProject.version
+val fullProjectName = "${rootProject.name}-${project.name}"
+
 base {
-    archivesName.set("JDA-opus-jna")
+    archivesName.set(fullProjectName)
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
+
+    withSourcesJar()
 }
 
 
@@ -75,8 +87,14 @@ dependencies {
 ////////////////////////////////////
 
 val jar by tasks.getting(Jar::class) {
-    archiveBaseName.set(project.name + "-opus-jna")
+    archiveBaseName.set(fullProjectName)
     manifest.attributes("Implementation-Version" to project.version, "Automatic-Module-Name" to "net.dv8tion.jda")
+}
+
+val javadocJar by tasks.registering(Jar::class) {
+    this.archiveBaseName.set(fullProjectName)
+    archiveClassifier.set("javadoc")
+    // Empty by design
 }
 
 tasks.withType<JavaCompile> {
@@ -91,4 +109,102 @@ tasks.withType<JavaCompile> {
     }
 
     options.compilerArgs.addAll(args)
+}
+
+////////////////////////////////////
+//                                //
+//    Publishing And Signing      //
+//                                //
+////////////////////////////////////
+
+
+// Generate pom file for maven central
+
+fun MavenPom.populate() {
+    packaging = "jar"
+    name.set(fullProjectName)
+    description.set("Opus support for JDA based on JNA")
+    url.set("https://github.com/discord-jda/JDA/tree/master/opus-jna")
+    scm {
+        url.set("https://github.com/discord-jda/JDA")
+        connection.set("scm:git:git://github.com/discord-jda/JDA")
+        developerConnection.set("scm:git:ssh:git@github.com:discord-jda/JDA")
+    }
+    licenses {
+        license {
+            name.set("The Apache Software License, Version 2.0")
+            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            distribution.set("repo")
+        }
+    }
+    developers {
+        developer {
+            id.set("Minn")
+            name.set("Florian Spieß")
+            email.set("business@minn.dev")
+        }
+        developer {
+            id.set("DV8FromTheWorld")
+            name.set("Austin Keener")
+            email.set("keeneraustin@yahoo.com")
+        }
+    }
+}
+
+val stagingDirectory = layout.buildDirectory.dir("staging-deploy").get()
+
+publishing {
+    publications {
+        register<MavenPublication>("Release") {
+            from(components["java"])
+
+            artifactId = fullProjectName
+            groupId = project.group as String
+            version = project.version as String
+
+            artifact(javadocJar)
+
+            pom.populate()
+        }
+    }
+
+    repositories.maven {
+        url = stagingDirectory.asFile.toURI()
+    }
+}
+
+jreleaser {
+    // Since this is a submodule
+    gitRootSearch = true
+
+    project {
+        versionPattern = "CUSTOM"
+    }
+
+    release {
+        github {
+            enabled = false
+        }
+    }
+
+    signing {
+        active = Active.RELEASE
+        armored = true
+    }
+
+    deploy {
+        maven {
+            mavenCentral {
+                register("sonatype") {
+                    active = Active.RELEASE
+                    url = "https://central.sonatype.com/api/v1/publisher"
+                    stagingRepository(stagingDirectory.asFile.relativeTo(projectDir).path)
+                }
+            }
+        }
+    }
+}
+
+tasks.withType<AbstractJReleaserTask>().configureEach {
+    mustRunAfter(tasks.named("publish"))
 }
