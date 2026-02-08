0a3686973138c47c1d3563699d8d548209ecbe31
Florian Spieß <business@minn.dev>
2026-02-08 17:44:28 +0100

    Update dependencies

---
+++ formatter-recipes/settings.gradle.kts
@@ -0,0 +1,9 @@
+rootProject.name = "formatter-recipes"
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
+}
