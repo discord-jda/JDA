9c06b8821d70bd217abb1aa6419c288593933f98
freya02 <41875020+freya022@users.noreply.github.com>
2026-02-24 13:04:25 +0100

    Add `Collectibles`

---
+++ src/main/java/net/dv8tion/jda/api/entities/Collectibles.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2015 Austin Keener, Michael Ritter, Florian Spieß, and the JDA contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.dv8tion.jda.api.entities;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+/**
+ * The collectibles a user has equipped.
+ *
+ * <p>This excludes avatar decorations and profile effects.
+ */
+public interface Collectibles {
+    /**
+     * Returns the equipped nameplate.
+     *
+     * @return The displayed nameplate
+     */
+    @Nullable
+    Nameplate getNameplate();
+
+    /**
+     * A decoration visible in Direct Messages and a guild's member list.
+     */
+    interface Nameplate {
+        /**
+         * Returns the SKU ID of this nameplate.
+         *
+         * <p>This is unique and will never change.
+         *
+         * @return The SKU ID
+         */
+        @Nonnull
+        String getSkuId();
+
+        /**
+         * Returns the asset path of this nameplate.
+         *
+         * <p>This is unique but not necessarily immutable.
+         *
+         * @return The asset path
+         */
+        @Nonnull
+        String getAssetPath();
+
+        /**
+         * Returns the name of the background color of this nameplate.
+         *
+         * @return Palette name of this nameplate's background color
+         *
+         * @see   <a href="https://docs.discord.com/developers/resources/user#nameplate" target="_blank">Official documentation for nameplates</a>
+         */
+        @Nonnull
+        String getPalette();
+    }
+}
---
+++ src/main/java/net/dv8tion/jda/internal/entities/CollectiblesImpl.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright 2015 Austin Keener, Michael Ritter, Florian Spieß, and the JDA contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.dv8tion.jda.internal.entities;
+
+import net.dv8tion.jda.api.entities.Collectibles;
+import net.dv8tion.jda.api.utils.data.DataObject;
+import net.dv8tion.jda.internal.utils.EntityString;
+
+import java.util.Objects;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+public class CollectiblesImpl implements Collectibles {
+    public static final Collectibles EMPTY = new CollectiblesImpl(DataObject.empty());
+
+    private final NameplateImpl nameplate;
+
+    private CollectiblesImpl(DataObject o) {
+        this.nameplate = o.optObject("nameplate").map(NameplateImpl::new).orElse(null);
+    }
+
+    @Nonnull
+    public static Collectibles extractFrom(@Nonnull DataObject o) {
+        if (o.isNull("collectibles")) {
+            return EMPTY;
+        }
+        DataObject object = o.getObject("collectibles");
+        // Avoid allocations if there are no collectibles
+        if (object.isNull("nameplate")) {
+            return EMPTY;
+        }
+        return new CollectiblesImpl(object);
+    }
+
+    @Nullable
+    @Override
+    public NameplateImpl getNameplate() {
+        return nameplate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof CollectiblesImpl)) {
+            return false;
+        }
+
+        CollectiblesImpl that = (CollectiblesImpl) o;
+        return Objects.equals(nameplate, that.nameplate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(nameplate);
+    }
+
+    @Override
+    public String toString() {
+        return new EntityString(this).addMetadata("nameplate", nameplate).toString();
+    }
+
+    public static class NameplateImpl implements Nameplate {
+        private final String sku, asset, palette;
+
+        public NameplateImpl(DataObject o) {
+            this.sku = o.getString("sku_id");
+            this.asset = o.getString("asset");
+            this.palette = o.getString("palette");
+        }
+
+        @Nonnull
+        @Override
+        public String getSkuId() {
+            return sku;
+        }
+
+        @Nonnull
+        @Override
+        public String getAssetPath() {
+            return asset;
+        }
+
+        @Nonnull
+        @Override
+        public String getPalette() {
+            return palette;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (!(o instanceof NameplateImpl)) {
+                return false;
+            }
+
+            NameplateImpl nameplate = (NameplateImpl) o;
+            return Objects.equals(sku, nameplate.sku)
+                    && Objects.equals(asset, nameplate.asset)
+                    && Objects.equals(palette, nameplate.palette);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(sku, asset, palette);
+        }
+
+        @Override
+        public String toString() {
+            return new EntityString(this)
+                    .addMetadata("asset_path", asset)
+                    .addMetadata("palette", palette)
+                    .toString();
+        }
+    }
+}
