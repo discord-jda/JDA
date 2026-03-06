16588cbf7cf9c9e487404b2bf625ff7e0a73d88a
freya02 <41875020+freya022@users.noreply.github.com>
2026-03-07 00:34:23 +0100

    Add voice channel effects

--- src/main/java/net/dv8tion/jda/internal/requests/WebSocketClient.java
+++ src/main/java/net/dv8tion/jda/internal/requests/WebSocketClient.java
@@ -1286,6 +1286,7 @@ public class WebSocketClient extends WebSocketAdapter implements WebSocketListen
         handlers.put("GUILD_ROLE_UPDATE", new GuildRoleUpdateHandler(api));
         handlers.put("GUILD_SYNC", new GuildSyncHandler(api));
         handlers.put("GUILD_STICKERS_UPDATE", new GuildStickersUpdateHandler(api));
+        handlers.put("VOICE_CHANNEL_EFFECT_SEND", new VoiceChannelEffectSendHandler(api));
         handlers.put("GUILD_UPDATE", new GuildUpdateHandler(api));
         handlers.put("INTERACTION_CREATE", new InteractionCreateHandler(api));
         handlers.put("INVITE_CREATE", new InviteCreateHandler(api));
