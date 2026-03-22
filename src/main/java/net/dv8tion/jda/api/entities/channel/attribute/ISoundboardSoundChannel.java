26381daf95ec7de690a69e50028409eef856f0db
freya02 <41875020+freya022@users.noreply.github.com>
2026-03-22 12:43:16 +0100

    Add `String` overload for `ISoundboardSoundChannel#sendSoundboardSound`

--- src/main/java/net/dv8tion/jda/api/entities/channel/attribute/ISoundboardSoundChannel.java
+++ src/main/java/net/dv8tion/jda/api/entities/channel/attribute/ISoundboardSoundChannel.java
@@ -22,6 +22,8 @@ import net.dv8tion.jda.api.entities.channel.middleman.AudioChannel;
 import net.dv8tion.jda.api.exceptions.InsufficientPermissionException;
 import net.dv8tion.jda.api.requests.ErrorResponse;
 import net.dv8tion.jda.api.requests.RestAction;
+import net.dv8tion.jda.api.utils.MiscUtil;
+import net.dv8tion.jda.internal.utils.Checks;
 
 import javax.annotation.CheckReturnValue;
 import javax.annotation.Nonnull;
@@ -47,6 +49,52 @@ public interface ISoundboardSoundChannel extends AudioChannel {
      *
      * @param  sound
      *         The sound to send
+     * @param  sourceGuildId
+     *         The guild ID from which this sound is from
+     *
+     * @throws InsufficientPermissionException
+     *         If the bot does not have the following permissions:
+     *         <ul>
+     *             <li>{@link Permission#VOICE_SPEAK VOICE_SPEAK}, {@link Permission#VOICE_USE_SOUNDBOARD VOICE_USE_SOUNDBOARD}</li>
+     *             <li>If the sound comes from other guilds, {@link Permission#VOICE_USE_EXTERNAL_SOUNDS VOICE_USE_EXTERNAL_SOUNDS} permission</li>
+     *         </ul>
+     * @throws IllegalArgumentException
+     *         If the provided sound is {@code null}, or the source guild ID is {@code null} or not a snowflake
+     * @throws IllegalStateException
+     *         <ul>
+     *             <li>If the bot is not connected to this channel</li>
+     *             <li>If the bot is deafened, muted or suppressed in this channel's guild</li>
+     *         </ul>
+     *
+     * @return {@link RestAction} - Type: {@link Void}
+     */
+    @Nonnull
+    @CheckReturnValue
+    default RestAction<Void> sendSoundboardSound(
+            @Nonnull SoundboardSoundSnowflake sound, @Nonnull String sourceGuildId) {
+        Checks.isSnowflake(sourceGuildId, "Source guild ID");
+        return sendSoundboardSound(sound, MiscUtil.parseSnowflake(sourceGuildId));
+    }
+
+    /**
+     * Sends this sound to this audio channel.
+     * <br>You must be connected to this audio channel to use this method,
+     * as well as be able to speak and hear.
+     *
+     * <p>The returned {@link RestAction} can encounter the following {@link ErrorResponse ErrorResponses}:
+     * <ul>
+     *     <li>{@link ErrorResponse#MISSING_PERMISSIONS MISSING_PERMISSIONS}
+     *     <br>The sound cannot be sent due to a permission discrepancy</li>
+     *     <li>{@link ErrorResponse#CANNOT_SEND_VOICE_EFFECT CANNOT_SEND_VOICE_EFFECT}
+     *     <br>The sound cannot be sent as the bot is either muted, deafened or suppressed</li>
+     *     <li>{@link ErrorResponse#UNKNOWN_SOUND UNKNOWN_SOUND}
+     *     <br>The sound was deleted/does not exist</li>
+     * </ul>
+     *
+     * @param  sound
+     *         The sound to send
+     * @param  sourceGuildId
+     *         The guild ID from which this sound is from
      *
      * @throws InsufficientPermissionException
      *         If the bot does not have the following permissions:
