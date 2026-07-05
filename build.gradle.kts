db8b79ed4540cae83d22ad302009fd90da51ee48
Florian Spieß <business@minn.dev>
2026-07-05 17:08:14 +0200

    Bump version to 6.5.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,7 +64,7 @@ val exampleJavaVersion = JavaLanguageVersion.of(25)
 val libraryJavaVersion = JavaLanguageVersion.of(8)
 
 projectEnvironment {
-    version = Version(major = "6", minor = "4", revision = "2", classifier = null)
+    version = Version(major = "6", minor = "5", revision = "0", classifier = null)
 }
 
 artifactFilters {
