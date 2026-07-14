46301de06cc27ac40410567e1d89023037351443
Yakov <officiallarffxx@mail.ru>
2026-07-14 17:28:24 +0300

    Update src/main/java/net/dv8tion/jda/internal/entities/subscription/SubscriptionImpl.java
    Co-authored-by: Florian Spieß <business@minn.dev>

--- src/main/java/net/dv8tion/jda/internal/entities/subscription/SubscriptionImpl.java
+++ src/main/java/net/dv8tion/jda/internal/entities/subscription/SubscriptionImpl.java
@@ -50,9 +50,9 @@ public class SubscriptionImpl implements Subscription {
 
         this.id = id;
         this.subscriberId = subscriberId;
-        this.skuIDs = skuIDs;
-        this.entitlementIDs = entitlementIDs;
-        this.renewalSkuIDs = renewalSkuIDs;
+        this.skuIDs = Collections.unmodifiableList(skuIDs);
+        this.entitlementIDs = Collections.unmodifiableList(entitlementIDs);
+        this.renewalSkuIDs = Collections.unmodifiableList(renewalSkuIDs);
         this.currentPeriodStart = currentPeriodStart;
         this.currentPeriodEnd = currentPeriodEnd;
         this.canceledAt = canceledAt;
