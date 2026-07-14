aac3110803bad2a348d6153b7f60351b08aa2770
Yakov <officiallarffxx@mail.ru>
2026-07-14 16:49:19 +0300

    Update src/main/java/net/dv8tion/jda/api/entities/subscription/Subscription.java
    Co-authored-by: Florian Spieß <business@minn.dev>

--- src/main/java/net/dv8tion/jda/api/entities/subscription/Subscription.java
+++ src/main/java/net/dv8tion/jda/api/entities/subscription/Subscription.java
@@ -87,7 +87,7 @@ public interface Subscription extends ISnowflake {
     }
 
     /**
-     * The renewal SKU id's related to this {@link Subscription}
+     * List of SKUs the user will be subsribed to on renewal
      *
      * @return The  renewal sku id's related to this {@link Subscription}
      */
